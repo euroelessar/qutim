b70a0cccc57744f334a0de37767c5db1b3842f11
Nigmatullin Ruslan <euroelessar@yandex.ru>
2009-09-25 12:49:38 +0000

    Added copyrights and licenses to files
    git-svn-id: http://qutim.org/svn/qutim/branches/sdk03@384 3b30d5e6-ffa4-dd11-8e54-00304894ce9e

--- libqutim/protocol.cpp
+++ libqutim/protocol.cpp
@@ -1,3 +1,18 @@
+/****************************************************************************
+ *  protocol.cpp
+ *
+ *  Copyright (c) 2009 by Nigmatullin Ruslan <euroelessar@gmail.com>
+ *
+ ***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************
+*****************************************************************************/
+
 #include "protocol.h"
 #include "account.h"
 #include "contact.h"
