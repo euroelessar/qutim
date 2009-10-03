ea19dab5c0444f46aa36abe7ca723e2786e0c07d
Sidorov Aleksey <sauron@citadelspb.com>
2009-10-03 13:57:51 +0000

    Reflections about SoundTheme API Popup backend API implementation
    git-svn-id: http://qutim.org/svn/qutim/branches/sdk03@392 3b30d5e6-ffa4-dd11-8e54-00304894ce9e

--- libqutim/notificationslayer.cpp
+++ libqutim/notificationslayer.cpp
@@ -1,22 +1,63 @@
 #include "notificationslayer.h"
+#include "libqutim_global.h"
+#include "objectgenerator.h"
 
 namespace qutim_sdk_0_3 {
 
-QPointer<NotificationsLayer> NotificationsLayer::instance = 0;
+	struct NotificationsLayerPrivate {
+		QPointer<PopupBackend> popup_backend;
+		QPointer<SoundBackend> sound_backend;
+		QList<QPointer<SoundThemeBackend> > sound_theme_backends;
+		const ObjectGenerator *gen;
+	};
 
-qutim_sdk_0_3::NotificationsLayer::NotificationsLayer()
-{
+	static NotificationsLayerPrivate *p = 0;
 
-}
+	void ensure_notifications_private_helper()
+	{
+		p = new NotificationsLayerPrivate;
+	}
 
-void qutim_sdk_0_3::NotificationsLayer::playSound(NotificationType type)
-{
+	inline void ensure_notifications_private()
+	{ if(!p) ensure_notifications_private_helper();}
 
-}
+	NotificationsLayer::NotificationsLayer()
+	{
 
-qutim_sdk_0_3::NotificationsLayer::~NotificationsLayer()
-{
+	}
 
-}
+	NotificationsLayer::~NotificationsLayer()
+	{
 
+	}
+
+	
+	void NotificationsLayer::sendNotification ( NotificationType type, QObject* sender, const QString& title, const QString& body )
+	{
+		ensure_notifications_private();
+		//TODO add checks
+		if (!isCoreInited())
+			return;
+		if (p->popup_backend.isNull())
+		{
+			GeneratorList popup_backends = moduleGenerators<PopupBackend>();
+			if(p->gen || p->gen = popup_backends.size() ? popup_backends.first() : 0)
+			{
+				p->popup_backend = p->gen->generate<PopupBackend>();
+			}
+		}
+		p->popup_backend->show(type,sender,title,body);
+	}
+
+
+	void NotificationsLayer::sendSimpleNotification ( const QString& title, const QString& body )
+	{
+		sendNotification(NotificationType::System,
+						 0,
+						 title,
+						 body);
+	}
+
+	
+	
 }
--- libqutim/notificationslayer.h
+++ libqutim/notificationslayer.h
@@ -4,29 +4,66 @@
 #include <QObject>
 #include <QPointer>
 
-namespace qutim_sdk_0_3 {
-    enum NotificationType {
-        //TODO
-    };
+namespace qutim_sdk_0_3
+{
+	enum NotificationType
+	{
+		//TODO
+		System,
+	};
 
-	class Contact;
-    class NotificationsLayer : public QObject
-    {
-        Q_OBJECT
-    public:
-        virtual void SendSimpleNotification(QString title, QString body = "") = 0;
-        virtual void SendNotification(NotificationType type,
-									  Contact contact,
-									  QString title,
-									  QString body = ""
-									  ) = 0;
-    protected:
-        NotificationsLayer();
-        virtual ~NotificationsLayer();
-        virtual void playSound(NotificationType type);
-    private:
-        static QPointer<NotificationsLayer> instance;
-    };
+	struct NotificationsLayerPrivate;
+	class PopupBackend : public QObject
+	{
+		virtual void show(NotificationType type,
+						  QObject *sender,
+						  const QString &title,
+						  const QString &body
+						  ) = 0;
+	};
+
+	class SoundBackend : public QObject
+	{
+		Q_OBJECT
+	public:
+		virtual void playSound (const QString &filename) = 0;
+	protected:
+		virtual void virtual_hook(int type, void *data());
+	};
+
+	class SoundThemeBackend : public QObject
+	{
+		//TODO
+		Q_OBJECT
+	public:
+		virtual bool loadTheme(const QString &path) = 0;
+	};
+
+	class SoundThemeProvider : public QObject
+	{
+		Q_OBJECT
+	public:
+		virtual bool init(const QString &path) = 0;
+		virtual void setSound(NotificationType sound, const QString &file) = 0;
+		virtual QString sound(NotificationType sound) = 0;
+	};
+
+	class NotificationsLayer : public QObject
+	{
+		Q_OBJECT
+	public:
+		virtual void sendSimpleNotification(const QString &title,const QString &body = QString());
+		virtual void sendNotification(NotificationType type,
+									QObject *sender,
+									const QString &title,
+									const QString &body = QString()
+									);
+	protected:
+		NotificationsLayer();
+		virtual ~NotificationsLayer();
+	private:
+		NotificationsLayerPrivate *p;
+	};
 
 }
 
