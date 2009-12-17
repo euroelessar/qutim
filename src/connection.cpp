fff4f17d17517ee7f001838ca60ca060fb17737c
Alexey Prokhin <alexey.prokhin@yandex.ru>
2009-12-17 15:26:01 +0300

    Rafactoring: OscarRate class moved to *.cpp

--- src/connection.cpp
+++ src/connection.cpp
@@ -19,6 +19,7 @@
 #include <QHostInfo>
 #include <QBuffer>
 #include <QDateTime>
+#include <QTimer>
 #include <QDebug>
 
 namespace Icq {
@@ -29,6 +30,40 @@ quint16 generate_flap_sequence()
 	for (quint32 i = n; i >>= 3; s += i);
 	return ((((0 - s) ^ (quint8)n) & 7) ^ n) + 2;
 }
+
+struct OscarRate: QObject
+{
+	Q_OBJECT
+public:
+	OscarRate(const SNAC &sn, AbstractConnection *conn);
+	void update(quint32 groupId, const SNAC &sn);
+	const QList<quint32> &snacTypes() { return m_snacTypes; }
+	void addSnacType(quint32 snacType) { m_snacTypes << snacType; }
+	quint16 groupId() { return m_groupId; }
+	void send(const SNAC &snac, bool priority);
+	bool isEmpty() { return m_windowSize <= 1; }
+private slots:
+	void sendNextPackets();
+private:
+	quint16 m_groupId;
+	quint32 m_windowSize;
+	quint32 m_clearLevel;
+	quint32 m_alertLevel;
+	quint32 m_limitLevel;
+	quint32 m_disconnectLevel;
+	quint32 m_currentLevel;
+	quint32 m_maxLevel;
+	quint32 m_lastTimeDiff;
+	quint8 m_currentState;
+	QDateTime m_time;
+	QList<quint32> m_snacTypes;
+	QList<SNAC> m_priorQueue;
+	QList<SNAC> m_queue;
+	double m_levelMultiplier;
+	double m_timeMultiplier;
+	QTimer m_timer;
+	AbstractConnection *m_conn;
+};
 
 ProtocolError::ProtocolError(const SNAC &snac)
 {
--- src/connection.h
+++ src/connection.h
@@ -20,8 +20,6 @@
 #include <QTcpSocket>
 #include <QMap>
 #include <QHostAddress>
-#include <QDateTime>
- #include <QTimer>
 #include <qutim/libqutim_global.h>
 #include "snachandler.h"
 #include "flap.h"
@@ -29,6 +27,8 @@
 namespace Icq {
 
 extern quint16 generate_flap_sequence();
+
+class OscarRate;
 
 struct ProtocolError
 {
@@ -41,40 +41,6 @@ private:
 	QString getErrorStr();
 };
 
-struct OscarRate: QObject
-{
-	Q_OBJECT
-public:
-	OscarRate(const SNAC &sn, AbstractConnection *conn);
-	void update(quint32 groupId, const SNAC &sn);
-	const QList<quint32> &snacTypes() { return m_snacTypes; }
-	void addSnacType(quint32 snacType) { m_snacTypes << snacType; }
-	quint16 groupId() { return m_groupId; }
-	void send(const SNAC &snac, bool priority);
-	bool isEmpty() { return m_windowSize <= 1; }
-private slots:
-	void sendNextPackets();
-private:
-	quint16 m_groupId;
-	quint32 m_windowSize;
-	quint32 m_clearLevel;
-	quint32 m_alertLevel;
-	quint32 m_limitLevel;
-	quint32 m_disconnectLevel;
-	quint32 m_currentLevel;
-	quint32 m_maxLevel;
-	quint32 m_lastTimeDiff;
-	quint8 m_currentState;
-	QDateTime m_time;
-	QList<quint32> m_snacTypes;
-	QList<SNAC> m_priorQueue;
-	QList<SNAC> m_queue;
-	double m_levelMultiplier;
-	double m_timeMultiplier;
-	QTimer m_timer;
-	AbstractConnection *m_conn;
-};
-
 class ProtocolNegotiation: public SNACHandler
 {
 	Q_OBJECT
@@ -85,7 +51,6 @@ private:
 	void setMsgChannelParams(AbstractConnection *conn, quint16 chans, quint32 flags);
 	quint32 m_login_reqinfo;
 };
-
 
 class AbstractConnection: public QObject
 {
