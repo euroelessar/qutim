9e48c1f57bda9ddd710c1d6bcf00f3ee1ddc2b1f
Nigmatullin Ruslan <euroelessar@yandex.ru>
2010-01-08 01:08:41 +0500

    Removed annoying WinMain function from main.cpp by linking with qtmain library

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-/*
-    main.cpp
-
-    Copyright (c) 2009 by Nigmatullin Ruslan <euroelessar@gmail.com>
-                  2009 by Aleksey Sidorov <sauron@citadelspb.com>
-
+/****************************************************************************
+ *  main.cpp
+ *
+ *  Copyright (c) 2009 by Nigmatullin Ruslan <euroelessar@gmail.com>
+ *                2009 by Aleksey Sidorov <sauron@citadelspb.com>
+ *
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -12,7 +12,7 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************
-*/
+*****************************************************************************/
 
 #include <QApplication>
 #include <QtCrypto>
@@ -42,24 +42,3 @@ int main(int argc, char *argv[])
 	srand(uint(qrand()));
 	return app.exec();
 }
-
-#ifdef Q_WS_WIN
-#include <windows.h>
-int __stdcall WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-{
-	LPWSTR *arg_w;
-	int argc;
-	arg_w = CommandLineToArgvW(GetCommandLineW(), &argc);
-	char **argv = new char *[argc];
-	for(int i=0;i<argc;i++)
-	{
-		int len=0;
-		while(!arg_w[i][len])
-			len++;
-		QByteArray data = QString::fromUtf16((ushort *)arg_w[i],len).toLocal8Bit();
-		argv[i] = (char *)qMalloc(data.size());
-		qMemCopy(argv[i], data.data(), data.size());
-	}
-	return main(argc, argv);
-}
-#endif
