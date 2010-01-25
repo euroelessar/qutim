2d13a55660eede14cc32477cf1c08989e50acb1d
Sidorov Aleksey <sauron@citadelspb.com>
2010-01-25 16:34:39 +0300

    New webkit style. Appearance Settings for popups should be created depending on their type

--- src/corelayers/kineticpopups/backend.cpp
+++ src/corelayers/kineticpopups/backend.cpp
@@ -23,7 +23,6 @@
 #include "popupwidget.h"
 #include <libqutim/configbase.h>
 #include "settings/popupappearance.h"
-#include "settings/popupbehavior.h"
 #include <libqutim/settingslayer.h>
 #include <libqutim/icon.h>
 
@@ -38,6 +37,8 @@ namespace KineticPopups
 
 	Backend::Backend ()
 	{
+		GeneralSettingsItem<Core::PopupAppearance> *appearance = new GeneralSettingsItem<Core::PopupAppearance>(Settings::Appearance, Icon("dialog-information"), QT_TRANSLATE_NOOP("Settings","Popups"));
+		Settings::registerItem(appearance);
 	}
 
 	void Backend::show(Notifications::Type type, QObject* sender, const QString& body,
