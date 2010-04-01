3f3b216c081590ce3e36441d2060c3246b5f5b6f
Alexey Prokhin <alexey.prokhin@yandex.ru>
2010-04-01 16:23:23 +0400

    Initial version of the information window about a contact.

---
+++ src/corelayers/contactinfo/contactinfo.h
@@ -0,0 +1,64 @@
+#ifndef SIMPLETRAY_H
+#define SIMPLETRAY_H
+
+#include "QWidget"
+#include "QGroupBox"
+#include "QScrollArea"
+#include <libqutim/buddy.h>
+#include <libqutim/inforequest.h>
+
+class QLabel;
+class QGridLayout;
+class QVBoxLayout;
+
+namespace Core
+{
+using namespace qutim_sdk_0_3;
+
+class InfoGroup : public QGroupBox
+{
+	Q_OBJECT
+public:
+	InfoGroup(QWidget *parent = 0);
+	void addItem(const InfoItem &item);
+private:
+	void addDataWidget(QWidget *widget);
+	QLabel *addLabel(const QString &data);
+	QGridLayout *m_layout;
+	int m_row;
+};
+
+class MainWindow : public QScrollArea
+{
+	Q_OBJECT
+public:
+	MainWindow();
+	void setBuddy(Buddy *buddy);
+private slots:
+	void onRequestStateChanged(InfoRequest::State state);
+private:
+	void addItems(const QList<InfoItem> &items);
+private:
+	InfoGroup *group(const LocalizedString &name);
+	QVBoxLayout *m_layout;
+	QHash<QString, InfoGroup*> m_groups;
+	InfoRequest *request;
+};
+
+class ContactInfo : public QObject
+{
+	Q_OBJECT
+	Q_CLASSINFO("Service", "ContactInfo")
+public:
+	ContactInfo();
+public slots:
+	void show(Buddy *buddy);
+private slots:
+	void onShow();
+private:
+	QPointer<MainWindow> info;
+};
+
+}
+
+#endif // SIMPLETRAY_H
