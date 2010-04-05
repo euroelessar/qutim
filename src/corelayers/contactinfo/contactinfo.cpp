32427d6df32f21f7a77762f3f688afea95a4235a
Alexey Prokhin <alexey.prokhin@yandex.ru>
2010-04-05 14:29:30 +0400

    Work on ContactInfo layer

--- src/corelayers/contactinfo/contactinfo.cpp
+++ src/corelayers/contactinfo/contactinfo.cpp
@@ -13,8 +13,10 @@
 #include "QVBoxLayout"
 #include <QDesktopWidget>
 #include <QApplication>
-
+#include <QHostAddress>
 #include "libqutim/debug.h"
+
+Q_DECLARE_METATYPE(QHostAddress);
 
 namespace Core
 {
@@ -53,34 +55,46 @@ void InfoGroup::addItem(const InfoItem &item)
 	m_layout->addWidget(title, m_row, 0, 1, 1, Qt::AlignRight | Qt::AlignTop);
 
 	QVariant::Type type = item.data().type();
-
 	if (type == QVariant::Date) {
 		addLabel(item.data().toDate().toString(Qt::SystemLocaleLongDate));
+		return;
 	} else if (type == QVariant::DateTime) {
 		addLabel(item.data().toDateTime().toString(Qt::SystemLocaleLongDate));
+		return;
 	} else if (type == QVariant::Image) {
 		QLabel *d = new QLabel(this);
 		d->setPixmap(QPixmap::fromImage(item.data().value<QImage>()));
 		addDataWidget(d);
+		return;
 	} else if (type == QVariant::Pixmap) {
 		QLabel *d = new QLabel(this);
 		d->setPixmap(item.data().value<QPixmap>());
 		addDataWidget(d);
+		return;
 	} else if (type == QVariant::Bool) {
 		addLabel(item.data().toBool() ?
 				 QT_TRANSLATE_NOOP("ContactInfo", "yes") :
 				 QT_TRANSLATE_NOOP("ContactInfo", "no"));
+		return;
+	} else if (item.data().canConvert<QHostAddress>()) {
+		QHostAddress address = item.data().value<QHostAddress>();
+		if (!address.isNull()) {
+			addLabel(address.toString());
+			return;
+		}
 	} else if (item.data().canConvert(QVariant::StringList)) {
-		foreach (const QString &i, item.data().toStringList())
-			addLabel(i);
-	} else {
-		QString str = item.data().toString();
-		bool enabled = !str.isEmpty();
-		if (!enabled)
-			str = QT_TRANSLATE_NOOP("ContactInfo", "the field is not set");
-		addLabel(str)->setEnabled(enabled);
+		QStringList list = item.data().toStringList();
+		if (!list.isEmpty()) {
+			foreach (const QString &i, list)
+				addLabel(i);
+			return;
+		}
 	}
-
+	QString str = item.data().toString();
+	bool enabled = !str.isEmpty();
+	if (!enabled)
+		str = QT_TRANSLATE_NOOP("ContactInfo", "the field is not set");
+	addLabel(str)->setEnabled(enabled);
 }
 
 void InfoGroup::addDataWidget(QWidget *widget)
