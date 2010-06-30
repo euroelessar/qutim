66715f06510e9b68ff9a727b9871384c07a0f080
zodiac <null@deltaz.org>
2010-06-30 16:46:56 +0600

    ProfileListWidget fix

--- src/profilelistwidget.cpp
+++ src/profilelistwidget.cpp
@@ -1,5 +1,6 @@
 #include "profilelistwidget.h"
 #include <QtGui/QGridLayout>
+#include <QtGui/QSpacerItem>
 
 namespace Core
 {
@@ -10,18 +11,21 @@ ProfileListWidget::ProfileListWidget(const QString &id, const QString &configDir
 	QLabel *idLabel = new QLabel();
 	passwordEdit = new QLineEdit();
 	loginButton = new QPushButton();
+	QSpacerItem *verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
 	connect(loginButton, SIGNAL(clicked()), this, SLOT(wantLogin()));
 	connect(passwordEdit, SIGNAL(returnPressed()), this, SLOT(wantLogin()));
 
 	photoLabel->setMinimumSize(QSize(64, 64));
 	photoLabel->setMaximumSize(QSize(64, 64));
+	passwordEdit->setEchoMode(QLineEdit::Password);
 	loginButton->setMinimumSize(QSize(22, 22));
 	loginButton->setMaximumSize(QSize(22, 22));
 	loginButton->setFocusPolicy(Qt::NoFocus);
-	gridLayout->addWidget(photoLabel, 0, 0, 2, 1);
-	gridLayout->addWidget(idLabel, 0, 1, 1, 2);
-	gridLayout->addWidget(passwordEdit, 1, 1, 1, 1);
-	gridLayout->addWidget(loginButton, 1, 2, 1, 1);
+	gridLayout->addWidget(photoLabel, 0, 0, 3, 1);
+	gridLayout->addWidget(idLabel, 0, 1, 1, 1);
+	gridLayout->addWidget(passwordEdit, 2, 1, 1, 1);
+	gridLayout->addWidget(loginButton, 2, 2, 1, 1);
+	gridLayout->addItem(verticalSpacer, 1, 1, 1, 1);
 	gridLayout->setContentsMargins(0, 0, 5, 5);
 
 	setLayout(gridLayout);
