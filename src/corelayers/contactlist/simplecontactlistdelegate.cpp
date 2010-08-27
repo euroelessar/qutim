a435cfa5a1f64765f4c98d5f5b91c110f7579b6a
Alexey Prokhin <alexey.prokhin@yandex.ru>
2010-08-27 16:47:02 +0400

    Refactoring in Status

--- src/corelayers/contactlist/simplecontactlistdelegate.cpp
+++ src/corelayers/contactlist/simplecontactlistdelegate.cpp
@@ -135,10 +135,6 @@ namespace SimpleContactList
 							icon = extIconVar.value<QIcon>();
 						if (!hash.value(QLatin1String("showIcon"),true).toBool() || icon.isNull())
 							continue;
-						if (!m_statuses.value(hash.value("id").toString(), true))
-							continue;
-						if (!m_statuses.value(map.value("id").toString(), true))
-							continue;
 						icon.paint(painter,
 									option.rect.left() + m_horizontal_padding,
 									option.rect.top() + m_vertical_padding,
