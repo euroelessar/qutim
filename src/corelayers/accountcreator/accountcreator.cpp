a23a0725852d4bb74f5aa8e77101331042c5b6c8
Sidorov Aleksey <sauron@citadelspb.com>
2010-08-28 14:22:47 +0400

    Unfinished

--- src/corelayers/accountcreator/accountcreator.cpp
+++ src/corelayers/accountcreator/accountcreator.cpp
@@ -6,50 +6,64 @@
 
 namespace Core
 {
-	AccountCreator::AccountCreator()
+	void AccountCreator::init()
 	{
+		LocalizedString name = QT_TRANSLATE_NOOP("Plugin","Account creator");
+		LocalizedString description = QT_TRANSLATE_NOOP("Plugin","Default qutIM account creator");
+		setInfo(name, description, QUTIM_VERSION);
+// 		addExtension(QT_TRANSLATE_NOOP("Plugin", "Account creator wizard"),
+// 				QT_TRANSLATE_NOOP("Plugin", "Account creator wizard"),
+// 				new GeneralGenerator<AccountPageCreator>()
+// 				);
 		SettingsItem *item = new GeneralSettingsItem<AccountCreatorList>(
 				Settings::General, Icon("meeting-attending"),
 				QT_TRANSLATE_NOOP("Settings", "Accounts"));
 		item->setPriority(100);
 		Settings::registerItem(item);
-		deleteLater();
+	}
 
+	bool AccountCreator::load()
+	{
 		foreach (Protocol *proto,allProtocols()) {
-			if (proto->accounts().count())
-				return;
+			if (!proto->accounts().isEmpty())
+				return true;
 		}
 
-		return; //fixme
+// 		AccountCreatorWizard *wizard = new AccountCreatorWizard();
+// 		wizard->setAttribute(Qt::WA_DeleteOnClose);
+// #if defined(QUTIM_MOBILE_UI)
+// 		wizard->showMaximized();
+// #else
+// 		centerizeWidget(wizard);
+// 		wizard->show();
+// #endif		
+		return true;
+	}
 
-		AccountCreatorWizard *wizard = new AccountCreatorWizard();
-		wizard->setAttribute(Qt::WA_DeleteOnClose);
-#if defined(QUTIM_MOBILE_UI)
-		wizard->showMaximized();
-#else
-		centerizeWidget(wizard);
-		wizard->show();
-#endif
-
+	bool AccountCreator::unload()
+	{
+		return true;
 	}
 
 	AccountCreator::~AccountCreator()
 	{
 	}
 		
-//	AccountPageCreator::AccountPageCreator(QObject* parent)
-//	{
-//		setParent(parent);
-//	}
-//	QList< QWizardPage* > AccountPageCreator::pages(QWidget* parent)
-//	{
-//		QList<QWizardPage *> list;
-//		list << new AccountCreatorProtocols(static_cast<QWizard*>(parent));
-//		return list;
-//	}
-//	double AccountPageCreator::priority() const
-//	{
-//		return 50;
-//	}
+	AccountPageCreator::AccountPageCreator(QObject* parent)
+	{
+		setParent(parent);
+	}
+	QList< QWizardPage* > AccountPageCreator::pages(QWidget* parent)
+	{
+		QList<QWizardPage *> list;
+		list << new AccountCreatorProtocols(qobject_cast<QWizard*>(parent));
+		return list;
+	}
+	double AccountPageCreator::priority() const
+	{
+		return 150;
+	}
 
 }
+
+QUTIM_EXPORT_PLUGIN(Core::AccountCreator)
--- src/corelayers/accountcreator/accountcreator.h
+++ src/corelayers/accountcreator/accountcreator.h
@@ -2,27 +2,30 @@
 #define ACCOUNTCREATOR_H
 
 #include <qutim/profilecreatorpage.h>
+#include <qutim/plugin.h>
 
 using namespace qutim_sdk_0_3;
 
 namespace Core
 {
-	class AccountCreator : public QObject
+	class AccountCreator : public Plugin
 	{
 		Q_OBJECT
 	public:
-		AccountCreator();
+		virtual void init();
+		virtual bool load();
+		virtual bool unload();
 		virtual ~AccountCreator();
 	};
 
-//	class AccountPageCreator : public ProfileCreatorPage
-//	{
-//		Q_OBJECT
-//	public:
-//		explicit AccountPageCreator(QObject *parent = 0);
-//		virtual double priority() const;
-//		virtual QList<QWizardPage *> pages(QWidget *parent);
-//	};
+	class AccountPageCreator : public ProfileCreatorPage
+	{
+		Q_OBJECT
+	public:
+		explicit AccountPageCreator(QObject *parent = 0);
+		virtual double priority() const;
+		virtual QList<QWizardPage *> pages(QWidget *parent);
+	};
 }
 
 #endif // ACCOUNTCREATOR_H
