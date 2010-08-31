60556352b8108c15f6181d12629456fdd666ded1
Sidorov Aleksey <sauron@citadelspb.com>
2010-09-01 02:57:49 +0400

    Fixed autoconnect

--- connectionmanager/src/connectionmanager.cpp
+++ connectionmanager/src/connectionmanager.cpp
@@ -9,80 +9,83 @@
 
 namespace ConnectionManager
 {
-	void changeState(Account *account, bool isOnline)
-	{
-		bool auto_connect = account->config().value("autoConnect",true);
-		//TODO
-		//Status last_status = account->config().value("lastStatus",Status(Status::Online));
-		if (isOnline) {
-			if (auto_connect) {
-				Status status = account->status();
-				status.setType(Status::Online);
-				account->setStatus(status);
-			}
-		}
-		else {
+void changeState(Account *account, bool isOnline)
+{
+	bool auto_connect = account->config().value("autoConnect",true);
+	//TODO
+	//Status last_status = account->config().value("lastStatus",Status(Status::Online));
+	if (isOnline) {
+		if (auto_connect) {
 			Status status = account->status();
-			status.setType(Status::Offline);
+			status.setType(Status::Online);
 			account->setStatus(status);
 		}
 	}
-
-	ConnectionManager::ConnectionManager() :
-		m_network_conf_manager(new QNetworkConfigurationManager(this))
-	{
+	else {
+		Status status = account->status();
+		status.setType(Status::Offline);
+		account->setStatus(status);
 	}
+}
 
-	ConnectionManager::~ConnectionManager()
-	{
+ConnectionManager::ConnectionManager() :
+	m_network_conf_manager(new QNetworkConfigurationManager(this))
+{
+}
 
-	}
+ConnectionManager::~ConnectionManager()
+{
 
-	void ConnectionManager::init()
-	{
-		debug() << Q_FUNC_INFO;
-		setInfo(QT_TRANSLATE_NOOP("Plugin", "ConnectionManager"),
-				QT_TRANSLATE_NOOP("Plugin", "Used to monitor the availability of network."),
-				PLUGIN_VERSION(0, 1, 0, 0));
-		addAuthor(QT_TRANSLATE_NOOP("Author","Aleksey Sidorov"),
-				  QT_TRANSLATE_NOOP("Task","Author"),
-				  QLatin1String("sauron@citadelspb.com"),
-				  QLatin1String("sauron.me"));
+}
 
-		connect(m_network_conf_manager.data(),SIGNAL(onlineStateChanged(bool)),SLOT(onOnlineStateChanged(bool)));
-	}
+void ConnectionManager::init()
+{
+	debug() << Q_FUNC_INFO;
+	setInfo(QT_TRANSLATE_NOOP("Plugin", "ConnectionManager"),
+			QT_TRANSLATE_NOOP("Plugin", "Used to monitor the availability of network."),
+			PLUGIN_VERSION(0, 1, 0, 0));
+	addAuthor(QT_TRANSLATE_NOOP("Author","Aleksey Sidorov"),
+			  QT_TRANSLATE_NOOP("Task","Author"),
+			  QLatin1String("sauron@citadelspb.com"),
+			  QLatin1String("sauron.me"));
 
-	bool ConnectionManager::load()
-	{
-		foreach (Protocol *protocol,allProtocols()) {
-			connect(protocol,
-					SIGNAL(accountCreated(qutim_sdk_0_3::Account*)),
-					SLOT(onAccountCreated(qutim_sdk_0_3::Account*)));
-		}
+	connect(m_network_conf_manager.data(),SIGNAL(onlineStateChanged(bool)),SLOT(onOnlineStateChanged(bool)));
+}
 
-		GeneralSettingsItem<ManagerSettings> *item = new GeneralSettingsItem<ManagerSettings>(Settings::Plugin, Icon("network-wireless"), QT_TRANSLATE_NOOP("Settings","Connection manager"));
-		Settings::registerItem(item);
-
-		return true;
-	}
-
-	bool ConnectionManager::unload()
-	{
-		return false;
-	}
-
-	void ConnectionManager::onOnlineStateChanged(bool isOnline)
-	{
-		foreach (Protocol *protocol,allProtocols()) {
-			foreach (Account *account,protocol->accounts())
-				changeState(account,isOnline);
+bool ConnectionManager::load()
+{
+	foreach (Protocol *protocol,allProtocols()) {
+		connect(protocol,
+				SIGNAL(accountCreated(qutim_sdk_0_3::Account*)),
+				SLOT(onAccountCreated(qutim_sdk_0_3::Account*)));
+		foreach (Account *a,protocol->accounts()) {
+			onAccountCreated(a);
 		}
 	}
 
-	void ConnectionManager::onAccountCreated(qutim_sdk_0_3::Account *account)
-	{
-		changeState(account,m_network_conf_manager->isOnline());
+	GeneralSettingsItem<ManagerSettings> *item = new GeneralSettingsItem<ManagerSettings>(Settings::Plugin, Icon("network-wireless"), QT_TRANSLATE_NOOP("Settings","Connection manager"));
+	Settings::registerItem(item);
+
+	return true;
+}
+
+bool ConnectionManager::unload()
+{
+	return false;
+}
+
+void ConnectionManager::onOnlineStateChanged(bool isOnline)
+{
+	foreach (Protocol *protocol,allProtocols()) {
+		foreach (Account *account,protocol->accounts())
+			changeState(account,isOnline);
 	}
+}
+
+void ConnectionManager::onAccountCreated(qutim_sdk_0_3::Account *account)
+{
+	changeState(account,m_network_conf_manager->isOnline());
+}
 
 }
 
