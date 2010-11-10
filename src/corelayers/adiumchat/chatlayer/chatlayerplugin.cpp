5b4433b82f0b1cc9a59dbbc113070de07a85d652
Sidorov Aleksey <sauron@citadelspb.com>
2010-11-10 16:00:34 +0300

    split chatlayer

--- src/corelayers/adiumchat/chatlayer/chatlayerplugin.cpp
+++ src/corelayers/adiumchat/chatlayer/chatlayerplugin.cpp
@@ -16,18 +16,16 @@
 
 #include "chatlayerplugin.h"
 #include "chatlayer/chatlayerimpl.h"
-#include "tabbedchatform/tabbedchatform.h"
-#include "stackedchatform/stackedchatform.h"
 #include "timemodifier/timemodifier.h"
 #include <qutim/icon.h>
 #include <qutim/servicemanager.h>
 #include <qutim/debug.h>
-#include "webkitchat/webkitviewfactory.h"
 #include <QWidgetAction>
 #include "chatlayer/actions/chatemoticonswidget.h"
 #include <QPlainTextEdit>
 #include "chatforms/abstractchatwidget.h"
 #include <qutim/shortcut.h>
+#include "chatforms/abstractchatform.h"
 
 namespace Core
 {
@@ -71,16 +69,6 @@ void ChatLayerPlugin::init()
 	addExtension<Core::AdiumChat::TimeModifier,Core::AdiumChat::MessageModifier>(
 				QT_TRANSLATE_NOOP("Plugin", "Time modifier"),
 				QT_TRANSLATE_NOOP("Plugin", "Modifier for %time% and %timeOpened% param, based on Apple's documentation"));
-	addExtension<Core::AdiumChat::TabbedChatForm>(
-				QT_TRANSLATE_NOOP("Plugin", "Classic"),
-				QT_TRANSLATE_NOOP("Plugin", "Default qutIM chatform implementation for adiumchat"));
-	addExtension<Core::AdiumChat::StackedChatForm>(
-				QT_TRANSLATE_NOOP("Plugin", "Mobile"),
-				QT_TRANSLATE_NOOP("Plugin", "Stacked chatform for mobile devices"));
-
-	addExtension<Core::AdiumChat::WebkitViewFactory>(
-				QT_TRANSLATE_NOOP("Plugin", "Webkit"),
-				QT_TRANSLATE_NOOP("Plugin", "Default qutIM chat implementation, based on Adium chat styles"));
 }
 
 bool ChatLayerPlugin::load()
