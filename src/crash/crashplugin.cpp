3cd3252685813769a7c5c2c17d5a96f143d19a2e
Sidorov Aleksey <sauron@citadelspb.com>
2010-12-02 17:54:20 +0300

    Fix for kde 4.5

--- src/crash/crashplugin.cpp
+++ src/crash/crashplugin.cpp
@@ -3,10 +3,15 @@
 #include <kglobal.h>
 #include <QApplication>
 #include <shared/shareddata.h>
+#include <kdeversion.h>
 
 KdeCrashHandler::KdeCrashHandler()
 {
 	KCrash::setApplicationPath( qApp->applicationDirPath() );
 	KCrash::setApplicationName( qAppName() );
-	KCrash::setCrashHandler( KCrash::defaultCrashHandler );
+#if KDE_VERSION_MINOR < 5
+	KCrash::setCrashHandler(KCrash::defaultCrashHandler);
+#else
+	KCrash::setDrKonqiEnabled (true);
+#endif
 }
