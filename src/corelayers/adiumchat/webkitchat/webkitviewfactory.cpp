a98a390adfa60956b62180df4b7351dc9fc949d2
Sidorov Aleksey <sauron@citadelspb.com>
2011-02-02 12:14:42 +0300

    Fixed strange dependency

--- src/corelayers/adiumchat/webkitchat/webkitviewfactory.cpp
+++ src/corelayers/adiumchat/webkitchat/webkitviewfactory.cpp
@@ -17,11 +17,11 @@ WebkitViewFactory::WebkitViewFactory()
 	m_appearanceSettings = new GeneralSettingsItem<ChatAppearance>(Settings::Appearance, Icon("view-choose"),
 																   QT_TRANSLATE_NOOP("Settings","Chat"));
 
-	//FIXME move to chatstyleoutput
-	QObject *obj = ServiceManager::getByName("ChatForm");
-	if (!obj) {
-		return;
-	}
+//	//FIXME move to chatstyleoutput
+//	QObject *obj = ServiceManager::getByName("ChatForm");
+//	if (!obj) {
+//		return;
+//	}
 	Settings::registerItem(m_appearanceSettings);
 }
 
