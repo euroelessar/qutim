2afe8c2fcbcccdb7e5b226beef1911ccf9f522c5
Aleksey <sauron@citadelspb.com>
2011-02-24 17:13:42 +0300

    Polishing

--- libqutim/contact.h
+++ libqutim/contact.h
@@ -21,76 +21,76 @@
 
 namespace qutim_sdk_0_3
 {
-	class Account;
-	class MetaContact;
-	class Message;
-	class ContactPrivate;
+class Account;
+class MetaContact;
+class Message;
+class ContactPrivate;
 
-	typedef QPair<LocalizedString, QVariant> InfoField;
-	typedef QList<InfoField> InfoFieldList;
+typedef QPair<LocalizedString, QVariant> InfoField;
+typedef QList<InfoField> InfoFieldList;
 
+/**
+ * @brief Contact is base class for all contactlist members
+ */
+class LIBQUTIM_EXPORT Contact : public Buddy
+{
+	Q_OBJECT
+	Q_DECLARE_PRIVATE(Contact)
+	Q_PROPERTY(QStringList tags READ tags WRITE setTags NOTIFY tagsChanged)
+	Q_PROPERTY(bool inList READ isInList WRITE setInList NOTIFY inListChanged)
+public:
 	/**
-	* @brief Contact is base class for all contactlist members
-	*/
-	class LIBQUTIM_EXPORT Contact : public Buddy
-	{
-		Q_OBJECT
-		Q_DECLARE_PRIVATE(Contact)
-		Q_PROPERTY(QStringList tags READ tags WRITE setTags NOTIFY tagsChanged)
-		Q_PROPERTY(bool inList READ isInList WRITE setInList NOTIFY inListChanged)
-	public:
-		/**
-		* @brief default Contact's contructor
-		*
-		* @param account Pointer to chatunit's account
-		*/
-		Contact(Account *account);
-		Contact(ContactPrivate &d, Account *account);
-		/**
-		* @brief Contact's destructor
-		*/
-		virtual ~Contact();
-		/**
-		* @brief Returns set of tags that mark the contact
-		*
-		* @return QSet<QString> tags
-		*/
-		virtual QStringList tags() const;
-		/**
-		* @brief set a set of tags
-		*
-		* @param tags
-		*/
-		virtual void setTags(const QStringList &tags) = 0;
-		/**
-		* @brief Returns membership contact to contactlist
-		*
-		* @return isInList
-		*/
-		virtual bool isInList() const = 0;
-		/**
-		* @brief set a membership contact to contactlist
-		*
-		* @param inList
-		*/
-		virtual void setInList(bool inList) = 0;
-		/**
-		* @brief add contact to contactlist, see also setInList(bool inList)
-		*/
-		void addToList() { setInList(true); }
-		/**
-		* @brief remove contact from contactlist, see also setInList(bool inList)
-		*/
-		void removeFromList() { setInList(false); }
-		virtual ChatUnit *upperUnit();
-	protected:
-		bool event(QEvent *);
-	signals:
-		void tagsChanged(const QStringList &current, const QStringList &previous);
-		void inListChanged(bool inList);
-	private:
-		friend class MetaContact;
-	};
+  * @brief default Contact's contructor
+  *
+  * @param account Pointer to chatunit's account
+  */
+	Contact(Account *account);
+	Contact(ContactPrivate &d, Account *account);
+	/**
+  * @brief Contact's destructor
+  */
+	virtual ~Contact();
+	/**
+  * @brief Returns set of tags that mark the contact
+  *
+  * @return QSet<QString> tags
+  */
+	virtual QStringList tags() const;
+	/**
+  * @brief set a set of tags
+  *
+  * @param tags
+  */
+	virtual void setTags(const QStringList &tags) = 0;
+	/**
+  * @brief Returns membership contact to contactlist
+  *
+  * @return isInList
+  */
+	virtual bool isInList() const = 0;
+	/**
+  * @brief set a membership contact to contactlist
+  *
+  * @param inList
+  */
+	virtual void setInList(bool inList) = 0;
+	/**
+  * @brief add contact to contactlist, see also setInList(bool inList)
+  */
+	void addToList() { setInList(true); }
+	/**
+  * @brief remove contact from contactlist, see also setInList(bool inList)
+  */
+	void removeFromList() { setInList(false); }
+	virtual ChatUnit *upperUnit();
+protected:
+	bool event(QEvent *);
+signals:
+	void tagsChanged(const QStringList &current, const QStringList &previous);
+	void inListChanged(bool inList);
+private:
+	friend class MetaContact;
+};
 }
 
 Q_DECLARE_METATYPE(qutim_sdk_0_3::Contact*)
