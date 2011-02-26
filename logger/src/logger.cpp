105c29378d9c0be01eb155f02ae249e6d18edd71
Sidorov Aleksey <sauron@citadelspb.com>
2011-02-26 17:00:37 +0300

    Polishing

--- logger/src/logger.cpp
+++ logger/src/logger.cpp
@@ -9,67 +9,67 @@
 
 namespace Logger
 {
-	using namespace std;
-	ofstream logfile;
+using namespace std;
+ofstream logfile;
 
-	void SimpleLoggingHandler(QtMsgType type, const char *msg) {
-		switch (type) {
-			case QtDebugMsg:
-				logfile << QTime::currentTime().toString().toLatin1().data() << " Debug: " << msg << "\n";
-				break;
-			case QtCriticalMsg:
-				logfile << QTime::currentTime().toString().toLatin1().data() << " Critical: " << msg << "\n";
-				break;
-			case QtWarningMsg:
-				logfile << QTime::currentTime().toString().toLatin1().data() << " Warning: " << msg << "\n";
-				break;
-			case QtFatalMsg:
-				logfile << QTime::currentTime().toString().toLatin1().data() <<  " Fatal: " << msg << "\n";
-				abort();
-			}
-		}
-
-	void LoggerPlugin::init()
-	{
-		debug() << Q_FUNC_INFO;
-		setInfo(QT_TRANSLATE_NOOP("Plugin", "Logger"),
-				QT_TRANSLATE_NOOP("Plugin", "Simple file based logger for debug"),
-				PLUGIN_VERSION(0, 1, 0, 0));
-		addAuthor(QT_TRANSLATE_NOOP("Author","Aleksey Sidorov"),
-				  QT_TRANSLATE_NOOP("Task","Author"),
-				  QLatin1String("sauron@citadelspb.com"),
-				  QLatin1String("sauron.me"));
+void SimpleLoggingHandler(QtMsgType type, const char *msg) {
+	switch (type) {
+	case QtDebugMsg:
+		logfile << QTime::currentTime().toString().toLatin1().data() << " Debug: " << msg << "\n";
+		break;
+	case QtCriticalMsg:
+		logfile << QTime::currentTime().toString().toLatin1().data() << " Critical: " << msg << "\n";
+		break;
+	case QtWarningMsg:
+		logfile << QTime::currentTime().toString().toLatin1().data() << " Warning: " << msg << "\n";
+		break;
+	case QtFatalMsg:
+		logfile << QTime::currentTime().toString().toLatin1().data() <<  " Fatal: " << msg << "\n";
+		abort();
 	}
+}
 
-	bool LoggerPlugin::load()
-	{
-		Config config = Config().group(QLatin1String("Logger"));
-		QString path = config.value(QLatin1String("path"),
-		                            SystemInfo::getPath(SystemInfo::ConfigDir).append("/qutim.log"));
-		int maxFileSize = config.value(QLatin1String("maxFileSize"), 512 * 1024);
-		QFileInfo fileInfo(path);
-		if (maxFileSize != -1 && fileInfo.size() > maxFileSize)
-			logfile.open(path.toLocal8Bit(), ios::out | ios::trunc);
-		else
-			logfile.open(path.toLocal8Bit(), ios::app);
-		qInstallMsgHandler(SimpleLoggingHandler);
-		debug() << tr("New session started, happy debuging ^_^");
+void LoggerPlugin::init()
+{
+	debug() << Q_FUNC_INFO;
+	setInfo(QT_TRANSLATE_NOOP("Plugin", "Logger"),
+			QT_TRANSLATE_NOOP("Plugin", "Simple file based logger for debug"),
+			PLUGIN_VERSION(0, 1, 0, 0));
+	addAuthor(QT_TRANSLATE_NOOP("Author","Aleksey Sidorov"),
+			  QT_TRANSLATE_NOOP("Task","Author"),
+			  QLatin1String("sauron@citadelspb.com"),
+			  QLatin1String("sauron.me"));
+}
 
-		AutoSettingsItem *settingsItem = new AutoSettingsItem(Settings::Plugin,
-															  Icon("view-choose"),
-															  QT_TRANSLATE_NOOP("Plugin", "Logger"));
-		settingsItem->setConfig(QString(),"Logger");
-		ObjectGenerator *gen = new GeneralGenerator<AutoSettingsFileChooser>();
-		AutoSettingsItem::Entry *entryItem = settingsItem->addEntry(QT_TRANSLATE_NOOP("Plugin", "Select log path"), gen);
-		entryItem->setProperty("path",path);
-		entryItem->setName("path");
-		Settings::registerItem(settingsItem);
-		return true;
-	}
-	bool LoggerPlugin::unload()
-	{
-		return false;
-	}
+bool LoggerPlugin::load()
+{
+	Config config = Config().group(QLatin1String("Logger"));
+	QString path = config.value(QLatin1String("path"),
+								SystemInfo::getPath(SystemInfo::ConfigDir).append("/qutim.log"));
+	int maxFileSize = config.value(QLatin1String("maxFileSize"), 512 * 1024);
+	QFileInfo fileInfo(path);
+	if (maxFileSize != -1 && fileInfo.size() > maxFileSize)
+		logfile.open(path.toLocal8Bit(), ios::out | ios::trunc);
+	else
+		logfile.open(path.toLocal8Bit(), ios::app);
+	qInstallMsgHandler(SimpleLoggingHandler);
+	debug() << tr("New session started, happy debuging ^_^");
+
+	AutoSettingsItem *settingsItem = new AutoSettingsItem(Settings::Plugin,
+														  Icon("view-choose"),
+														  QT_TRANSLATE_NOOP("Plugin", "Logger"));
+	settingsItem->setConfig(QString(),"Logger");
+	ObjectGenerator *gen = new GeneralGenerator<AutoSettingsFileChooser>();
+	AutoSettingsItem::Entry *entryItem = settingsItem->addEntry(QT_TRANSLATE_NOOP("Plugin", "Select log path"), gen);
+	entryItem->setProperty("path",path);
+	entryItem->setName("path");
+	Settings::registerItem(settingsItem);
+	return true;
+}
+bool LoggerPlugin::unload()
+{
+	return false;
+}
 }
 
 QUTIM_EXPORT_PLUGIN(Logger::LoggerPlugin)
--- logger/src/logger.h
+++ logger/src/logger.h
@@ -6,17 +6,17 @@
 namespace Logger
 {
 
-	using namespace qutim_sdk_0_3;
+using namespace qutim_sdk_0_3;
 
-	class LoggerPlugin : public Plugin
-	{
-		Q_OBJECT
-		Q_CLASSINFO("DebugName", "Logger")
-	public:
-		virtual void init();
-		virtual bool load();
-		virtual bool unload();
-	};
+class LoggerPlugin : public Plugin
+{
+	Q_OBJECT
+	Q_CLASSINFO("DebugName", "Logger")
+public:
+	virtual void init();
+	virtual bool load();
+	virtual bool unload();
+};
 
 }
 #endif // LOGGER_H
