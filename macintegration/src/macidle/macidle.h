ba57cf495d5c67b7b02d1ac36461746e3b8660f3
Sidorov Aleksey <sauron@citadelspb.com>
2011-04-28 03:44:11 +0400

    Separate to pluginset

---
+++ macintegration/src/macidle/macidle.h
@@ -0,0 +1,37 @@
+#ifndef MACIDLE_H
+#define MIDLE_H
+
+#include <QObject>
+#include <qutim/protocol.h>
+
+namespace MacIntegration
+{
+using namespace qutim_sdk_0_3;
+
+struct MacIdlePrivate;
+
+class MacIdle : public QObject
+{
+	Q_OBJECT
+	Q_CLASSINFO("Service", "AutoAway")
+	Q_DECLARE_PRIVATE(MacIdle)
+	public:
+		enum Reason {
+			Screensaver,
+			Away,
+			Inactive,
+			NoIdle
+		};
+		MacIdle();
+		~MacIdle();
+		void setIdleOn(Reason reason);
+		void setIdleOff();
+		void reloadSettings();
+	private slots:
+		void onTimeout();
+	private:
+		QScopedPointer<MacIdlePrivate> d_ptr;
+};
+}
+
+#endif // MACIDLE_H
---
+++ macintegration/src/macspellchecker/macspellchecker.h
@@ -0,0 +1,29 @@
+#ifndef MACSPELLCHECKER_H
+#define MACSPELLCHECKER_H
+
+#include <qutim/spellchecker.h>
+
+namespace MacIntegration
+{
+using namespace qutim_sdk_0_3;
+
+class MacSpellChecker : public SpellChecker
+{
+	Q_OBJECT
+	public:
+		MacSpellChecker();
+		~MacSpellChecker();
+		virtual bool isCorrect(const QString &word) const;
+		virtual QStringList suggest(const QString &word) const;
+		virtual void store(const QString &word) const;
+		virtual void storeReplacement(const QString &bad, const QString &good);
+		QStringList languages();
+		void loadSettings(const QString &lang);
+		static MacSpellChecker *instance() { Q_ASSERT(self); return self; }
+		static QString toPrettyLanguageName(const QString &lang);
+	private:
+		static MacSpellChecker *self;
+};
+}
+
+#endif // MACSPELLCHECKER_H
