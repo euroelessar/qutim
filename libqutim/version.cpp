2f7a85289c4792d531391d794615274da19d5198
Ruslan Nigmatullin <euroelessar@yandex.ru>
2011-05-03 22:47:47 +0600

    Added forgotten files

---
+++ libqutim/version.cpp
@@ -0,0 +1,49 @@
+/****************************************************************************
+ *  version.cpp
+ *
+ *  Copyright (c) 2011 by Nigmatullin Ruslan <euroelessar@gmail.com>
+ *
+ ***************************************************************************
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************
+*****************************************************************************/
+
+#include "version.h"
+
+namespace qutim_sdk_0_3
+{
+quint8 versionMajor()
+{
+	return QUTIM_VERSION_MAJOR;
+}
+
+quint8 versionMinor()
+{
+	return QUTIM_VERSION_MINOR;
+}
+
+quint8 versionSecMinor()
+{
+	return QUTIM_VERSION_SECMINOR;
+}
+
+quint8 versionPatch()
+{
+	return QUTIM_VERSION_PATCH;
+}
+
+quint32 version()
+{
+	return QUTIM_VERSION;
+}
+
+const char* versionString()
+{
+	return QUTIM_VERSION_STRING;
+}
+}
