ed933550f5f480c41779806eee1ba7f1dcdc4315
Sidorov Aleksey <sauron@citadelspb.com>
2011-05-07 14:10:24 +0400

    Code style fix

--- src/corelayers/idlestatuschanger/idlestatuschanger.cpp
+++ src/corelayers/idlestatuschanger/idlestatuschanger.cpp
@@ -13,79 +13,79 @@ Core::IdleStatusChanger* pIdleStatusChanger = 0;
 
 namespace Core
 {
-	IdleStatusChanger::IdleStatusChanger() :
-			m_awayStatus(Status::Away), m_naStatus(Status::NA)
-	{
-		QObject *idle = ServiceManager::getByName("Idle");
-		reloadSettings();
-		m_state = Active;
-		connect(idle, SIGNAL(secondsIdle(int)), this, SLOT(onIdle(int)));
-		SettingsItem* settings = new GeneralSettingsItem<IdleStatusWidget>(
+IdleStatusChanger::IdleStatusChanger() :
+	m_awayStatus(Status::Away), m_naStatus(Status::NA)
+{
+	QObject *idle = ServiceManager::getByName("Idle");
+	reloadSettings();
+	m_state = Active;
+	connect(idle, SIGNAL(secondsIdle(int)), this, SLOT(onIdle(int)));
+	SettingsItem* settings = new GeneralSettingsItem<IdleStatusWidget>(
 				Settings::General,
 				Icon("user-away-extended"),
 				QT_TRANSLATE_NOOP("AutoAway", "Auto-away"));
-		Settings::registerItem(settings);
-		pIdleStatusChanger = this;
+	Settings::registerItem(settings);
+	pIdleStatusChanger = this;
 
-		m_awayStatus.setProperty("changeReason",Status::ByIdle);
-		m_naStatus.setProperty("changeReason",Status::ByIdle);
-	}
+	m_awayStatus.setProperty("changeReason",Status::ByIdle);
+	m_naStatus.setProperty("changeReason",Status::ByIdle);
+}
 
-	void IdleStatusChanger::refillAccounts()
-	{
-		foreach (Protocol *proto, Protocol::all()) {
-			foreach (Account *acc, proto->accounts()) {
-				if (m_accounts.contains(acc)
+void IdleStatusChanger::refillAccounts()
+{
+	foreach (Protocol *proto, Protocol::all()) {
+		foreach (Account *acc, proto->accounts()) {
+			if (m_accounts.contains(acc)
 					|| acc->status() == Status::Offline
 					|| acc->status() == Status::Invisible
 					|| acc->status() == Status::DND
 					|| acc->status() == Status::NA)
-					continue;
-				m_accounts.append(acc);
-				m_statuses.append(acc->status());
-			}
+				continue;
+			m_accounts.append(acc);
+			m_statuses.append(acc->status());
 		}
 	}
+}
 
-	void IdleStatusChanger::onIdle(int secs)
-	{
-		if ((m_awayEnabled?m_state == Away:1)
-			 && secs > m_naSecs
-			 && m_naEnabled) {
-			refillAccounts();
-			foreach (Account *acc, m_accounts) {
-				if (acc)
-					acc->setStatus(m_naStatus);
-			}
-			m_state = Inactive;
-		} else if (m_state == Active
-					  && secs > m_awaySecs
-					  && m_awayEnabled) {
-			refillAccounts();
-			foreach (Account *acc, m_accounts) {
-				if (acc)
-					acc->setStatus(m_awayStatus);
-			}
-			m_state = Away;
-		} else if (m_state != Active && secs < m_awaySecs) {
-			for (int i = 0; i < m_accounts.size(); i++) {
-				if (m_accounts.at(i))
-					m_accounts.at(i)->setStatus(m_statuses.at(i));
-			}
-			m_accounts.clear();
-			m_statuses.clear();
-			m_state = Active;
+void IdleStatusChanger::onIdle(int secs)
+{
+	if ((m_awayEnabled?m_state == Away:1)
+			&& secs > m_naSecs
+			&& m_naEnabled) {
+		refillAccounts();
+		foreach (Account *acc, m_accounts) {
+			if (acc)
+				acc->setStatus(m_naStatus);
 		}
+		m_state = Inactive;
+	} else if (m_state == Active
+			   && secs > m_awaySecs
+			   && m_awayEnabled) {
+		refillAccounts();
+		foreach (Account *acc, m_accounts) {
+			if (acc)
+				acc->setStatus(m_awayStatus);
+		}
+		m_state = Away;
+	} else if (m_state != Active && secs < m_awaySecs) {
+		for (int i = 0; i < m_accounts.size(); i++) {
+			if (m_accounts.at(i))
+				m_accounts.at(i)->setStatus(m_statuses.at(i));
+		}
+		m_accounts.clear();
+		m_statuses.clear();
+		m_state = Active;
 	}
+}
 
-	void IdleStatusChanger::reloadSettings()
-	{
-		Config conf(AA_CONFIG_GROUP);
-		m_awayEnabled = conf.value("away-enabled", true);
-		m_naEnabled   = conf.value("na-enabled",   true);
-		m_awaySecs = conf.value("away-secs", AWAY_DEF_SECS);
-		m_naSecs   = conf.value("na-secs",   NA_DEF_SECS);
-		m_awayStatus.setText(conf.value("away-text", ""));
-		m_naStatus.  setText(conf.value("na-text",   ""));
-	}
+void IdleStatusChanger::reloadSettings()
+{
+	Config conf(AA_CONFIG_GROUP);
+	m_awayEnabled = conf.value("away-enabled", true);
+	m_naEnabled   = conf.value("na-enabled",   true);
+	m_awaySecs = conf.value("away-secs", AWAY_DEF_SECS);
+	m_naSecs   = conf.value("na-secs",   NA_DEF_SECS);
+	m_awayStatus.setText(conf.value("away-text", ""));
+	m_naStatus.  setText(conf.value("na-text",   ""));
+}
 }
