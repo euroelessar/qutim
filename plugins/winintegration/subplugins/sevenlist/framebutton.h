aadd5b1e626ca81aee4ea235a1083e46ad3a2d5d
Ivan V <define-true-false@yandex.com>
2011-06-09 16:01:10 +0300

    Added licence header to sevenwidget plugin files.

--- plugins/winintegration/subplugins/sevenlist/framebutton.h
+++ plugins/winintegration/subplugins/sevenlist/framebutton.h
@@ -1,3 +1,28 @@
+/****************************************************************************
+**
+** qutIM instant messenger
+**
+** Copyright (C) 2011 Aleksey Sidorov <sauron@citadelspb.com>
+**
+*****************************************************************************
+**
+** $QUTIM_BEGIN_LICENSE$
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program. If not, see http://www.gnu.org/licenses/.
+** $QUTIM_END_LICENSE$
+**
+****************************************************************************/
+
 #ifndef FRAMEBUTTON_H
 #define FRAMEBUTTON_H
 
@@ -6,7 +31,7 @@
 class FrameButtonPrivate;
 class FrameButton : public QPushButton
 {
-    Q_OBJECT
+	 Q_OBJECT
 	Q_DECLARE_PRIVATE(FrameButton)
 public:
 	explicit FrameButton(QWidget *parent = 0);
--- plugins/winintegration/subplugins/sevenlist/toolframewindow.cpp
+++ plugins/winintegration/subplugins/sevenlist/toolframewindow.cpp
@@ -1,18 +1,29 @@
 /****************************************************************************
- *  toolframewindow.cpp
- *
- *  Copyright (c) 2011 by Sidorov Aleksey <sauron@citadelspb.com>
- *  Copyright (c) 2011 by Vizir Ivan <define-true-false@yandex.com >
- *
- ***************************************************************************
- *                                                                         *
- *   This library is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************
-*****************************************************************************/
+**
+** qutIM instant messenger
+**
+** Copyright (C) 2011 Aleksey Sidorov <sauron@citadelspb.com>
+** Copyright (C) 2011 Ivan Vizir <define-true-false@yandex.com>
+**
+*****************************************************************************
+**
+** $QUTIM_BEGIN_LICENSE$
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 2 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program. If not, see http://www.gnu.org/licenses/.
+** $QUTIM_END_LICENSE$
+**
+****************************************************************************/
+
 #include "toolframewindow_p.h"
 #include <QLibrary>
 #include <QStyle>
