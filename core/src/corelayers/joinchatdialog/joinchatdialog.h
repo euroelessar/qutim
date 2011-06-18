e6387799ba49f78d0547b80214d406b0f9153fed
Ruslan Nigmatullin <euroelessar@yandex.ru>
2011-06-18 23:59:31 +0600

    Added join chat dialog for Desktop

---
+++ core/src/corelayers/joinchatdialog/joinchatdialog.h
@@ -0,0 +1,77 @@
+/****************************************************************************
+**
+** qutIM instant messenger
+**
+** Copyright (C) 2008 Denis Daschenko <daschenko@gmail.com>
+** Copyright (C) 2011 Ruslan Nigmatullin <euroelessar@ya.ru>
+**
+*****************************************************************************
+**
+** $QUTIM_BEGIN_LICENSE$
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+** $QUTIM_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef JJOINCHAT_H
+#define JJOINCHAT_H
+
+#include <qutim/groupchatmanager.h>
+#include <qutim/dataforms.h>
+#include <qutim/status.h>
+#include <QDialog>
+
+class QListWidgetItem;
+
+namespace Ui
+{
+class JoinChat;
+}
+
+class JoinChatDialog : public QDialog
+{
+	Q_OBJECT
+
+	public:
+		JoinChatDialog(QWidget *parent = 0);
+		~JoinChatDialog();
+
+	public slots:
+		void showConference(QListWidgetItem *current, QListWidgetItem *previous);
+		void on_addConferenceButton_clicked();
+		void on_removeConferenceButton_clicked();
+		void on_accountBox_currentIndexChanged(int index);
+
+	protected:
+		bool eventFilter(QObject *obj, QEvent *event);
+
+	private slots:
+		void onAccountCreated(qutim_sdk_0_3::Account *account, bool first = true);
+		void onAccountStatusChanged(const qutim_sdk_0_3::Status &status);
+		void onManagerChanged(qutim_sdk_0_3::GroupChatManager *manager);
+		void onAccountDeath(QObject *object);
+		void joinConference();
+		void on_saveButton_clicked();
+		void joinBookmark(QListWidgetItem *item);
+
+	private:
+		qutim_sdk_0_3::GroupChatManager *groupChatManager();
+
+		Ui::JoinChat *m_ui;
+		QWeakPointer<qutim_sdk_0_3::AbstractDataForm> m_dataForm;
+		qutim_sdk_0_3::Account *m_currentAcount;
+
+};
+
+#endif //JJOINCHAT_H
---
+++ core/src/corelayers/joinchatdialog/joinchatmodule.h
@@ -0,0 +1,63 @@
+/****************************************************************************
+**
+** qutIM instant messenger
+**
+** Copyright (С) 2010 Sidorov Aleksey <sauron@citadelspb.com>
+** Copyright (C) 2011 Ruslan Nigmatullin <euroelessar@ya.ru>
+**
+*****************************************************************************
+**
+** $QUTIM_BEGIN_LICENSE$
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+** $QUTIM_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef JOINCHATMODULE_H
+#define JOINCHATMODULE_H
+
+#include "joinchatdialog.h"
+#include <qutim/actiongenerator.h>
+
+namespace Core
+{
+class JoinChatGenerator;
+
+class JoinChatModule : public QObject
+{
+	Q_OBJECT
+	Q_CLASSINFO("Service", "JoinGroupChat")
+	Q_CLASSINFO("Uses", "IconLoader")
+	Q_CLASSINFO("Uses", "ContactList")
+public:
+	JoinChatModule();
+	virtual ~JoinChatModule();
+
+public slots:
+	void onJoinChatTriggered();
+
+private:
+	QScopedPointer<JoinChatGenerator> m_action;
+	QWeakPointer<JoinChatDialog> m_dialog;
+};
+
+class JoinChatGenerator : public qutim_sdk_0_3::ActionGenerator
+{
+public:
+	JoinChatGenerator(QObject *module);
+	void showImpl(QAction *action, QObject *obj);
+};
+}
+
+#endif // JOINCHATMODULE_H
