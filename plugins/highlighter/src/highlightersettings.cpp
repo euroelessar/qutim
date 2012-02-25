a74f0564bb07faa8009b58070bc9c94e39f78a7a
Nicolay Izoderov <nico-izo@yandex.ru>
2012-02-25 17:29:54 +0400

    simple fix

--- plugins/highlighter/src/highlightersettings.cpp
+++ plugins/highlighter/src/highlightersettings.cpp
@@ -2,7 +2,7 @@
 **
 ** qutIM - instant messenger
 **
-** Copyright © 2012 Nicolay Izoderov <nico-izo@yandex.ru>
+** Copyright © 2012 Nicolay Izoderov <nico-izo@ya.ru>
 **
 *****************************************************************************
 **
