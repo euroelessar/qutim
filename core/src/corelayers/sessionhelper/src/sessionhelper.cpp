c4a392f33d45e31fd557914b609bfe074fa10e82
Nicolay Izoderov <nico-izo@yandex.ru>
2012-02-26 22:39:52 +0400

    changed addAuthor in sessionhelper

--- core/src/corelayers/sessionhelper/src/sessionhelper.cpp
+++ core/src/corelayers/sessionhelper/src/sessionhelper.cpp
@@ -39,10 +39,7 @@ void SessionHelper::init()
 			QT_TRANSLATE_NOOP("Plugin", "Activates the session when certain events"),
 			PLUGIN_VERSION(0, 1, 0, 0));
 	setCapabilities(Loadable);
-	addAuthor(QT_TRANSLATE_NOOP("Author","Aleksey Sidorov"),
-			  QT_TRANSLATE_NOOP("Task","Developer"),
-			  QLatin1String("gorthauer87@yandex.ru"),
-			  QLatin1String("sauron.me"));
+	addAuthor(QLatin1String("sauron"));
 }
 
 bool SessionHelper::load()
