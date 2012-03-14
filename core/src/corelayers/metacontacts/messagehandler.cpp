1f720a1593e832f42d53dd575c9a8df6390d15e6
Sergei Lopatin <magist3r@gmail.com>
2012-03-14 16:36:13 +0600

    Fixed active contact setting.

--- core/src/corelayers/metacontacts/messagehandler.cpp
+++ core/src/corelayers/metacontacts/messagehandler.cpp
@@ -17,37 +17,20 @@ MetaContactMessageHandler::MetaContactMessageHandler()
 
 MessageHandler::Result MetaContactMessageHandler::doHandle(qutim_sdk_0_3::Message &message, QString *reason)
 {
-    if (message.isIncoming())
-    {
-        MetaContactImpl *contact = qobject_cast<MetaContactImpl*>(message.chatUnit()->metaContact());
-        if(contact)
-        {
-            debug() << "Metacontact: " << contact;
-            Contact* active = contact->getActiveContact();
-            QList<Contact*> contacts = contact->contacts();
-            debug() << "Active contact: " << active;
-            if(active->buddy() != message.chatUnit()->buddy())
-            {
-                for (int i = 0; i < contacts.size(); i++)
-                {
-                    if (contacts.at(i)->account() == message.chatUnit()->account())
-                    {
-                        contact->setActiveContact(contacts.at(i));
-                        return MetaContactMessageHandler::Accept;
-                    }
-                }
-            }
-        }
-        debug() << message.chatUnit();
-        debug() << message.chatUnit()->buddy();
-        debug() << message.chatUnit()->account();
-        debug() << message.chatUnit()->account()->protocol();
-        debug() << message.text();
-    }
-
-
-
-    return MetaContactMessageHandler::Accept;
+	if (message.isIncoming()) {
+		if (MetaContactImpl *contact = qobject_cast<MetaContactImpl*>(message.chatUnit()->metaContact())) {
+			Contact *rawContact = 0;
+			ChatUnit *u = message.chatUnit();
+			while (u) {
+			   if ((rawContact = qobject_cast<Contact*>(u)))
+						break;
+					u = u->upperUnit();
+			}
+			if (rawContact && contact->getActiveContact()->buddy() != rawContact)
+				contact->setActiveContact(rawContact);
+		}
+	}
+	return MetaContactMessageHandler::Accept;
 
 }
 
