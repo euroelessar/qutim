f0dc0d636d47da1315e2188e203e184a24da7010
ide user ide_hamper <hamper.nhs@gmail.com>
2012-05-02 20:55:11 +0000

    Значок авторизации для oscar протокола

--- protocols/oscar/src/authorization.cpp
+++ protocols/oscar/src/authorization.cpp
@@ -30,6 +30,7 @@
 #include <qutim/notification.h>
 #include <QApplication>
 #include <qutim/servicemanager.h>
+#include <qutim/tooltip.h>
 
 namespace qutim_sdk_0_3 {
 
@@ -160,6 +161,22 @@ void Authorization::onAuthChanged(IcqContact *contact, bool auth)
 	contact->setProperty("authorizedBy", auth);
 	foreach (QAction *action, m_authActionGen->actions(contact))
 		action->setText(authActionText(auth));
+    Status status = contact->status();
+    if (auth)
+        status.removeExtendedInfo("authorization");
+    else {
+        QVariantHash extStatus;
+        extStatus.insert("id", "authorization");
+        extStatus.insert("title", QObject::tr("Not authorized"));
+        extStatus.insert("icon", QVariant::fromValue(ExtensionIcon("dialog-warning")));
+        extStatus.insert("description", QString());
+        extStatus.insert("showInTooltip", true);
+        extStatus.insert("priorityInContactList", 80);
+        extStatus.insert("priorityInTooltip", 30);
+        extStatus.insert("iconPosition", qVariantFromValue(ToolTipEvent::IconBeforeDescription));
+        status.setExtendedInfo("authorization", extStatus);
+    }
+    contact->setStatus(status, false);
 }
 
 } } // namespace qutim_sdk_0_3::oscar
