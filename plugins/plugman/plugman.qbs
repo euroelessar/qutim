9c33efb6b7e7df874748c3e29d0ce31c188fc5da
Aleksey Sidorov <gorthauer87@ya.ru>
2013-01-12 14:36:01 +0400

    Linking with libarchive instead dlopen

--- plugins/plugman/plugman.qbs
+++ plugins/plugman/plugman.qbs
@@ -5,6 +5,7 @@ UreenPlugin {
 
     Depends { name: "artwork" }
     Depends { name: "attica" }
+    Depends { name: "archive" }
     Depends { name: 'qt.declarative' }
     Depends { name: 'qt.opengl' }
 
--- plugins/plugman/src/plugmanarchive.cpp
+++ plugins/plugman/src/plugmanarchive.cpp
@@ -3,75 +3,24 @@
 #include <QDir>
 #include <QLibrary>
 
-#define DEFINE_FUNCTION(RETURN_TYPE, NAME, ARGUMENTS) \
-    typedef RETURN_TYPE (*NAME ## _) ARGUMENTS; \
-    static NAME ## _ NAME = NULL;
-
-#define RESOLVE_FUNCTION(NAME) \
-    NAME = reinterpret_cast<NAME ## _>(lib.resolve(#NAME)); \
-    if (!NAME) \
-        return false
-
-#define RESOLVE_FUNCTION_2(NAME, NAME2) \
-    NAME = reinterpret_cast<NAME ## _>(lib.resolve(#NAME)); \
-    if (!NAME) \
-        NAME = reinterpret_cast<NAME ## _>(lib.resolve(#NAME2)); \
-    if (!NAME) \
-        return false
-
-namespace PlugmanLibArchive {
+extern "C" {
+Q_DECL_IMPORT struct archive* archive_read_new();
+Q_DECL_IMPORT int archive_read_data_block(struct archive *a,
+                                          const void **buff, size_t *size, qint64 *offset);
+Q_DECL_IMPORT int archive_read_support_compression_all(struct archive *);
+Q_DECL_IMPORT int archive_read_support_format_all(struct archive *);
+Q_DECL_IMPORT int archive_read_open_file(struct archive *,
+                                         const char *_filename, size_t _block_size);
+Q_DECL_IMPORT int archive_read_next_header(struct archive *,
+                                           struct archive_entry **);
+Q_DECL_IMPORT const wchar_t * archive_entry_pathname_w(struct archive_entry *);
+Q_DECL_IMPORT int archive_read_free(struct archive *);
+Q_DECL_IMPORT int archive_read_close(struct archive *);
+Q_DECL_IMPORT const char *archive_error_string(struct archive *);
+}
 
 #define	ARCHIVE_EOF	  1
 #define	ARCHIVE_OK	  0
-
-DEFINE_FUNCTION(struct archive *, archive_read_new, ())
-DEFINE_FUNCTION(int, archive_read_data_block, (struct archive *a, const void **buff, size_t *size, qint64 *offset))
-DEFINE_FUNCTION(int, archive_read_support_compression_all, (struct archive *))
-DEFINE_FUNCTION(int, archive_read_support_format_all, (struct archive *))
-DEFINE_FUNCTION(int, archive_read_open_file, (struct archive *, const char *_filename, size_t _block_size))
-DEFINE_FUNCTION(int, archive_read_next_header, (struct archive *, struct archive_entry **))
-DEFINE_FUNCTION(const wchar_t *, archive_entry_pathname_w, (struct archive_entry *))
-DEFINE_FUNCTION(int, archive_read_free, (struct archive *))
-DEFINE_FUNCTION(int, archive_read_close, (struct archive *))
-DEFINE_FUNCTION(const char	*, archive_error_string, (struct archive *))
-
-
-static bool initLibArchive_helper()
-{
-    QLibrary lib(QLatin1String("archive"));
-    if (!lib.load()) {
-        lib.setFileName(QLatin1String("libarchive"));
-        if (!lib.load()) {
-            return false;
-        }
-    }
-    RESOLVE_FUNCTION(archive_read_new);
-    RESOLVE_FUNCTION(archive_read_data_block);
-    RESOLVE_FUNCTION(archive_read_support_compression_all);
-    RESOLVE_FUNCTION(archive_read_support_format_all);
-    RESOLVE_FUNCTION(archive_read_open_file);
-    RESOLVE_FUNCTION(archive_read_next_header);
-    RESOLVE_FUNCTION(archive_entry_pathname_w);
-    RESOLVE_FUNCTION_2(archive_read_free, archive_read_finish);
-    RESOLVE_FUNCTION(archive_read_close);
-    RESOLVE_FUNCTION(archive_error_string);
-    return true;
-}
-
-static bool initLibArchive()
-{
-    static bool inited = false;
-    static bool result = false;
-    if (!inited) {
-        result = initLibArchive_helper();
-        inited = true;
-    }
-    return result;
-}
-
-}
-
-using namespace PlugmanLibArchive;
 
 PlugmanArchive::PlugmanArchive()
 {
@@ -109,10 +58,6 @@ static bool plugman_copy_data(struct archive *ar, const QDir &dir, const QString
 
 bool PlugmanArchive::extract(const QString &file, const QString &path, QString *error)
 {
-    if (!initLibArchive()) {
-        *error = QLatin1String("Can not load \"libarchive\"");
-        return false;
-    }
     const QDir dir = path;
     const bool doExtract = true;
     const bool verbose = true;
