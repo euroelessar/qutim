e98985d68d0c3e09dfb1a7f4a222f2fac0f3afd9
Ruslan Nigmatullin <euroelessar@yandex.ru>
2013-01-28 23:11:13 +0400

    [YandexNarod] Moved to Yandex.Disk

--- plugins/yandexnarod/src/yandexnaroduploadjob.cpp
+++ plugins/yandexnarod/src/yandexnaroduploadjob.cpp
@@ -38,91 +38,10 @@
 
 using namespace qutim_sdk_0_3;
 
-YandexNarodBuffer::YandexNarodBuffer(const QString &fileName, QIODevice *file,
-									 const QByteArray &boundary, QObject *parent) :
-	QIODevice(parent)
-{
-	QByteArray data;
-	data.append("--").append(boundary).append("\r\n");
-	data.append("Content-Disposition: form-data; name=\"file\"; filename=\"")
-			.append(fileName).append("\"\r\n");
-	data.append("Content-Transfer-Encoding: binary\r\n");
-	data.append("\r\n");
-
-	QBuffer *buffer = new QBuffer(this);
-	buffer->setData(data);
-	m_devices.append(buffer);
-
-	m_devices.append(file);
-	file->setParent(this);
-	connect(file, SIGNAL(destroyed()), SLOT(deleteLater()));
-
-	data.clear();
-	data.append("\r\n--").append(boundary).append("--\r\n");
-
-	buffer = new QBuffer(this);
-	buffer->setData(data);
-	m_devices.append(buffer);
-}
-
-YandexNarodBuffer::~YandexNarodBuffer()
-{
-}
-
-bool YandexNarodBuffer::open(OpenMode mode)
-{
-	bool result = true;
-	foreach (QIODevice *device, m_devices)
-		result &= device->open(mode);
-	QIODevice::open(mode);
-	return result;
-}
-
-qint64 YandexNarodBuffer::size() const
-{
-	quint64 totalSize = 0;
-	foreach (QIODevice *device, m_devices)
-		totalSize += device->size();
-	return totalSize;
-}
-
-qint64 YandexNarodBuffer::readData(char *data, qint64 maxlen)
-{
-	qint64 totalSize = 0;
-	foreach (QIODevice *device, m_devices) {
-		if (maxlen == 0 || device->atEnd())
-			continue;
-		qint64 currentSize = device->read(data, maxlen);
-		totalSize += currentSize;
-		maxlen -= currentSize;
-		data += currentSize;
-	}
-	return totalSize;
-}
-
-qint64 YandexNarodBuffer::writeData(const char *data, qint64 len)
-{
-	Q_UNUSED(data);
-	Q_UNUSED(len);
-	return -1;
-}
-
 YandexNarodUploadJob::YandexNarodUploadJob(qutim_sdk_0_3::ChatUnit *contact,
 										   YandexNarodFactory *factory) :
 	FileTransferJob(contact, Outgoing, factory)
 {
-	m_timer.setInterval(1000);
-	m_timer.setSingleShot(true);
-	connect(&m_timer, SIGNAL(timeout()), this, SLOT(someStrangeSlot()));
-
-#if HAS_NO_TOKEN_AUTHORIZATION
-	m_request.setRawHeader("Cache-Control", "no-cache");
-	m_request.setRawHeader("Accept", "*/*");
-	QByteArray userAgent = "qutIM/";
-	userAgent += versionString();
-	userAgent += " (U; YB/4.2.0; MRA/5.5; en)";
-	m_request.setRawHeader("User-Agent", userAgent);
-#endif
 }
 
 YandexNarodUploadJob::~YandexNarodUploadJob()
@@ -144,9 +63,7 @@ void YandexNarodUploadJob::doSend()
 
 void YandexNarodUploadJob::doStop()
 {
-	// TODO: I am not sure that it's correct
-	m_timer.stop();
-	delete m_data.data();
+	// TODO: Implement
 }
 
 void YandexNarodUploadJob::doReceive()
@@ -156,23 +73,36 @@ void YandexNarodUploadJob::doReceive()
 
 void YandexNarodUploadJob::sendImpl()
 {
-	setStateString(QT_TR_NOOP("Getting storage..."));
-#if HAS_NO_TOKEN_AUTHORIZATION
-	m_request.setUrl(QUrl("http://narod.yandex.ru/disk/getstorage/?type=json"));
+	setStateString(QT_TR_NOOP("Uploading file..."));
 
-//	QNetworkCookieJar *cookieJar = YandexNarodFactory::networkManager()->cookieJar();
-//	foreach(const QNetworkCookie &cookie, cookieJar->cookiesForUrl(m_request.url())) {
-//		m_request.setRawHeader("Cookie", cookie.toRawForm(QNetworkCookie::NameAndValueOnly));
-//		debug() << cookie;
-//	}
+	m_data = setCurrentIndex(0);
 
-//	debug() << "Cookie" << m_request.rawHeader("Cookie");
-	QNetworkReply *reply = YandexNarodFactory::networkManager()->get(m_request);
-#else
-	YandexRequest request(QUrl("http://narod.yandex.ru/disk/getstorage/?type=json"));
-	QNetworkReply *reply = YandexNarodFactory::networkManager()->get(request);
-#endif
-	connect(reply, SIGNAL(finished()), this, SLOT(storageReply()));
+	if (!m_data->open(QIODevice::ReadOnly)) {
+		setError(IOError);
+		setErrorString(tr("Could not open file %1").arg(fileName()));
+		return;
+	}
+
+	QString fileName;
+	// TODO: Think about better solution
+//	fileName += QString::number(qrand(), 16);
+//	fileName += "-";
+	fileName += YandexNarodUploadJob::fileName();
+
+	QUrl url(QLatin1String("https://webdav.yandex.ru/"));
+	url.setPath(fileName);
+
+	YandexRequest request(QUrl("https://webdav.yandex.ru/" + fileName));
+	request.setRawHeader("Content-Length", QByteArray::number(m_data->size()));
+	request.setRawHeader("Content-Type", "application/octet-stream");
+	request.setRawHeader("Expect", "100-continue");
+
+	QNetworkReply *reply = YandexNarodFactory::networkManager()->put(request, m_data);
+	connect(reply, SIGNAL(finished()), this, SLOT(onUploadFinished()));
+	connect(reply, SIGNAL(uploadProgress(qint64,qint64)),
+			this, SLOT(onUploadProgress(qint64,qint64)));
+
+	setState(Started);
 }
 
 bool YandexNarodUploadJob::processReply(QNetworkReply *reply)
@@ -197,168 +127,75 @@ void YandexNarodUploadJob::authorizationResult(YandexNarodAuthorizator::Result r
 	}
 }
 
-void YandexNarodUploadJob::storageReply()
+void YandexNarodUploadJob::onUploadProgress(qint64 bytesSent, qint64 bytesTotal)
+{
+	Q_UNUSED(bytesTotal);
+	setFileProgress(bytesSent);
+}
+
+void YandexNarodUploadJob::onUploadFinished()
 {
 	QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
 	Q_ASSERT(reply);
-	if (!processReply(reply))
-		return;
+	reply->deleteLater();
 
-	QByteArray data = reply->readAll();
-	QVariantMap map = m_someData = Json::parse(data).toMap();
-
-	debug() << "storage" << map;
-	debug() << "storage" << reply->rawHeader("Set-Cookie");
-
-	QUrl url(map.value("url").toString());
-	url.addQueryItem("tid", map.value("hash").toString());
-
-	int boundaryTemp[] = { qrand(), qrand(), qrand() };
-	QByteArray boundary = QByteArray::fromRawData(reinterpret_cast<char *>(boundaryTemp),
-												  sizeof(boundaryTemp)).toHex();
-	m_data = setCurrentIndex(0);
-	m_data = new YandexNarodBuffer(fileName(), m_data.data(), boundary, this);
-	if (!m_data.data()->open(QIODevice::ReadOnly)) {
-		setError(IOError);
-		setErrorString(tr("Could not open file %1").arg(fileName()));
+	if (reply->error() != QNetworkReply::NoError) {
+		setState(Error);
+		setError(NetworkError);
 		return;
 	}
 
-	setState(Started);
-	
-#if HAS_NO_TOKEN_AUTHORIZATION
-	QNetworkRequest request(m_request);
-	request.setUrl(url);
-#else
+	int code = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+
+	if (code == 507) {
+		setState(Error);
+		setError(NetworkError);
+		setErrorString(QT_TR_NOOP("Not anough space on storage"));
+		return;
+	} else if (code == 413) {
+		setState(Error);
+		setError(NetworkError);
+		setErrorString(QT_TR_NOOP("File too large"));
+		return;
+	} else if (code != 201) {
+		setState(Error);
+		setError(NetworkError);
+		return;
+	}
+
+	QUrl url = reply->url();
+	url.setEncodedQuery("publish");
 	YandexRequest request(url);
-#endif
-	request.setRawHeader("Content-Type", "multipart/form-data, boundary=" + boundary);
-	request.setRawHeader("Content-Length", QString::number(m_data.data()->size()).toLatin1());
+	QNetworkReply *postReply = YandexNarodFactory::networkManager()->post(request, QByteArray());
+	connect(postReply, SIGNAL(finished()), SLOT(onPublishFinished()));
 
-	QNetworkReply *uploadNetworkReply = YandexNarodFactory::networkManager()->post(request, m_data.data());
-	connect(m_data.data(), SIGNAL(destroyed()), uploadNetworkReply, SLOT(deleteLater()));
-	connect(uploadNetworkReply, SIGNAL(finished()), this, SLOT(uploadReply()));
-
-	m_timer.start();
+	setStateString(QT_TR_NOOP("Publishing file..."));
 }
 
-void YandexNarodUploadJob::someStrangeSlot()
-{
-	QUrl url(m_someData.value("purl").toString());
-	url.addQueryItem("tid", m_someData.value("hash").toString());
-//	url.addQueryItem("type", "json");
-#if HAS_NO_TOKEN_AUTHORIZATION
-	m_request.setUrl(url);
-	QNetworkReply *reply = YandexNarodFactory::networkManager()->get(m_request);
-#else
-	YandexRequest request(url);
-	QNetworkReply *reply = YandexNarodFactory::networkManager()->get(request);
-#endif
-	connect(m_data.data(), SIGNAL(destroyed()), reply, SLOT(deleteLater()));
-	connect(reply, SIGNAL(finished()), SLOT(progressReply()));
-}
-
-void YandexNarodUploadJob::uploadReply()
-{
-	m_timer.stop();
-	debug() << "uploadReply";
-	QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
-	Q_ASSERT(reply);
-	if (!processReply(reply))
-		return;
-
-	QByteArray data = reply->readAll();
-	QVariantMap map = Json::parse(data).toMap();
-
-	debug() << "upload" << data << map;
-	debug() << reply->rawHeaderList();
-	debug() << "upload" << reply->rawHeader("Set-Cookie");
-
-	someStrangeSlot();
-}
-
-void YandexNarodUploadJob::progressReply()
+void YandexNarodUploadJob::onPublishFinished()
 {
 	QNetworkReply *reply = qobject_cast<QNetworkReply *>(sender());
 	Q_ASSERT(reply);
-	if (!processReply(reply))
-		return;
+	reply->deleteLater();
 
-	QByteArray data = reply->readAll();
-#if HAS_NO_TOKEN_AUTHORIZATION
-	int start = data.indexOf('{');
-	int end = data.lastIndexOf('}');
-	QVariantMap map = Json::parse(data.mid(start, end - start + 1)).toMap();
-#else
-	QVariant tmpVar;
-	int dataBegin = data.indexOf('(') + 1;
-	int dataEnd = data.lastIndexOf(')');
-	int dataLength = dataEnd - dataBegin;
-	if (dataBegin < 0 || dataEnd < 0 || dataLength < 0) {
-		dataBegin = 0;
-		dataLength = data.length();
-	}
-	Json::parseRecord(tmpVar, data.constData() + dataBegin, &dataLength);
-	QVariantMap map = tmpVar.toMap();
-#endif
+	QUrl url = reply->header(QNetworkRequest::LocationHeader).toUrl();
+	if (url.isRelative())
+		url = reply->url().resolved(url);
 
-	debug() << "progress" << reply->request().url() << data << map;
-	debug() << "progress" << reply->rawHeader("Set-Cookie");
-	int newProgress = fileSize() * (map.value("percent").toDouble() / 100.0);
-	if (newProgress > progress())
-		setFileProgress(newProgress);
+	Config config;
+	config.beginGroup(QLatin1String("yandex/narod"));
+	QString text = config.value("template", tr("File sent: %N (%S bytes)\n%U",
+											   "Don't remove format arguments"));
+	text.replace("%N", fileName());
+	text.replace("%U", url.toString());
+	text.replace("%S", QString::number(m_data->size()));
 
-	QString status = map.value("status").toString();
-	if (status == "upload") {
-		m_timer.start();
-//		{
-//		"tid": "1263553646SvsF9FFimF5thmoM2kGTwxL9",
-//		"status": "upload",
-//		"percent": "94.11",
-//		"ipercent": 94,
-//		"random": "1263553668S4gHxbB2nJSd2clhhfYimsfM",
-//		"time": { "min": "0","sec": "01" }
-//		}
-	} else if (status == "done" && state() != Finished) {
-//		{
-//		"tid": "1263553646SvsF9FFimF5thmoM2kGTwxL9",
-//		"status": "done",
-//		"fids": "8433938",
-//		"files": [{ "fid": "8433938", "hash": "16928325000", "name": "Makefile", "size": "146305" }],
-//		"percent": "100.00",
-//		"ipercent": 100,
-//		"random": "1263553670cshMPkykOsHbaCnIQ5ehEOKd",
-//		"time": { "min": "0","sec": "00" }
-//		}
-
-		setState(Finished);
-		QVariantMap varMap = map.value("files").toList().value(0).toMap();
-		if (!varMap.isEmpty() && chatUnit()) {
-			debug() << "done" << chatUnit()->title() << varMap;
-
-			QString url(QLatin1String("http://narod.ru/disk/"));
-			url += varMap.value("hash").toString();
-			url += QLatin1Char('/');
-			url += varMap.value("name").toString();
-			url += QLatin1String(".html");
-			debug() << url;
-
-			QString sendmsg = Config().group("yandex").group("narod")
-							  .value("template", tr("File sent: %N (%S bytes)\n%U",
-													"Don't remove format arguments"));
-			sendmsg.replace("%N", varMap.value("name").toString());
-			sendmsg.replace("%U", url);
-			sendmsg.replace("%S", varMap.value("size").toString());
-
-			//Append message to chatunit
-			Message msg(sendmsg);
-			msg.setIncoming(false);
-			msg.setProperty("service", true);
-			if (ChatSession *s = ChatLayer::get(chatUnit(), false))
-				s->appendMessage(msg);
-			msg.setProperty("service", false);
-			chatUnit()->account()->getUnitForSession(chatUnit())->send(msg);
-		}
-	}
+	Message message(text);
+	message.setIncoming(false);
+	message.setProperty("service", true);
+	if (ChatSession *s = ChatLayer::get(chatUnit(), false))
+		s->appendMessage(message);
+	message.setProperty("service", false);
+	chatUnit()->account()->getUnitForSession(chatUnit())->send(message);
 }
 
