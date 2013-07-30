ff74bea9b117a59fdb4764e52b88a4c8c558fed1
Ruslan Nigmatullin <euroelessar@yandex.ru>
2013-07-30 21:55:44 +0400

    Added forgotten macro to dbusnotification plugin

--- plugins/dbusnotifications/src/dplugin.h
+++ plugins/dbusnotifications/src/dplugin.h
@@ -32,6 +32,7 @@ using namespace qutim_sdk_0_3;
 class DPlugin : public Plugin
 {
 	Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qutim.Plugin")
 	Q_CLASSINFO("DebugName", "Freedesktop")
 public:
 			DPlugin();
