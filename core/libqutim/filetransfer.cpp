27034157f071bc3484bd969478f0be829bfdddfa
trett <roman@trett.ru>
2013-07-30 21:06:17 +0400

    [filetransfer] small fix

--- core/libqutim/filetransfer.cpp
+++ core/libqutim/filetransfer.cpp
@@ -431,9 +431,10 @@ void FileTransferJob::setFileProgress(qint64 fileProgress)
 {
 	Q_D(FileTransferJob);
 	qint64 delta = fileProgress - d->fileProgress;
-//	Q_ASSERT(delta >= 0);
-//	d->fileProgress = fileProgress;
-//	d->progress += delta;
+	if (fileProgress > 0) fileProgress = 0;
+	Q_ASSERT(delta >= 0);
+	d->fileProgress = fileProgress;
+	d->progress += delta;
 	emit progressChanged(d->progress);
 }
 
