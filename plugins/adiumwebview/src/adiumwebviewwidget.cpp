bca143f23d14d9a2627e31ba1d50ea6100d68cea
trett <roman@trett.ru>
2013-08-27 19:13:55 +0400

    [adiumwebview] context menu for quote added

--- plugins/adiumwebview/src/adiumwebviewwidget.cpp
+++ plugins/adiumwebview/src/adiumwebviewwidget.cpp
@@ -29,6 +29,9 @@
 #include <qutim/servicemanager.h>
 #include <QWebFrame>
 #include <QApplication>
+#include <QMenu>
+#include <QContextMenuEvent>
+#include <QPlainTextEdit>
 #ifdef Q_WS_MAEMO_5
 #include <QMouseEvent>
 #endif
@@ -48,6 +51,8 @@ WebViewWidget::WebViewWidget(QWidget *parent)
 	mousePressed = false;
 	installEventFilter(this);
 #endif
+	setContextMenuPolicy(Qt::CustomContextMenu);
+	connect(this, SIGNAL(customContextMenuRequested(QPoint)), SLOT(showCustomContextMenu(QPoint)));
 }
 
 void WebViewWidget::setViewController(QObject* object)
@@ -90,6 +95,34 @@ bool WebViewWidget::eventFilter(QObject *, QEvent *e)
     return false;
 }
 #endif
+
+void WebViewWidget::showCustomContextMenu(const QPoint &point)
+{
+	QMenu *menu = page()->createStandardContextMenu();
+	if(!selectedHtml().isEmpty()) {
+		QAction *quote = new QAction(tr("&Quote"), this);
+		menu->addAction(quote);
+		connect(quote, SIGNAL(triggered()), SLOT(insertQuoteText()));
+	}
+	menu->popup(mapToGlobal(point));
+}
+
+void WebViewWidget::insertQuoteText()
+{
+	QString text;
+	const QString newLine = QLatin1String("\n> ");
+	QString quote = m_controller->quote();
+	quote.prepend("> ");
+	for (int i = 0; i < quote.size(); ++i) {
+		if (quote[i] == QLatin1Char('\n') || quote[i].unicode() == QChar::ParagraphSeparator)
+			text += newLine;
+		else
+			text += quote[i];
+	}
+	text.reserve(text.size() + quote.size() * 1.2);
+	text += QLatin1Char('\n');
+	m_controller->appendText(text);
+}
 
 } // namespace Adium
 
