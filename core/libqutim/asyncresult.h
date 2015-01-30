a1b5999f28ec29a75e8ff45d6ce7deb9a5d8742f
Ruslan Nigmatullin <euroelessar@yandex.ru>
2015-01-30 15:58:41 -0800

    [Core] Removed useless LIBQUTIM_EXPORT

--- core/libqutim/asyncresult.h
+++ core/libqutim/asyncresult.h
@@ -57,7 +57,7 @@ public slots:
 };
 
 template <typename... Args>
-class LIBQUTIM_EXPORT AsyncResultData
+class AsyncResultData
 {
 	typedef std::tuple<Args...> Tuple;
     typedef std::shared_ptr<Tuple> TuplePtr;
