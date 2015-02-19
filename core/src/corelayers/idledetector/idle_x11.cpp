e8ebe2645868ad28bd3d3ba0222ea888e3f04a36
Aleksey Sidorov <aleksey.sidorov@dev.zodiac.tv>
2015-02-19 13:24:21 +0300

    Compile fixes on linux

--- core/src/corelayers/idledetector/idle_x11.cpp
+++ core/src/corelayers/idledetector/idle_x11.cpp
@@ -37,7 +37,7 @@ static xcb_screen_t * screen;
 
 IdlePlatform::IdlePlatform()
 {
-	screen = xcb_setup_roots_iterator (xcb_get_setup (QX11Info::connection())).data;
+    screen = xcb_setup_roots_iterator (xcb_get_setup (QX11Info::connection())).data;
 }
 
 IdlePlatform::~IdlePlatform()
@@ -47,18 +47,18 @@ IdlePlatform::~IdlePlatform()
 
 bool IdlePlatform::init()
 {
-	return true;
+    return true;
 }
 
 int IdlePlatform::secondsIdle()
 {
-	xcb_screensaver_query_info_cookie_t cookie = xcb_screensaver_query_info (QX11Info::connection(), screen->root);
-	xcb_screensaver_query_info_reply_t *info = xcb_screensaver_query_info_reply (QX11Info::connection(), cookie, NULL);
+    xcb_screensaver_query_info_cookie_t cookie = xcb_screensaver_query_info (QX11Info::connection(), screen->root);
+    xcb_screensaver_query_info_reply_t *info = xcb_screensaver_query_info_reply (QX11Info::connection(), cookie, NULL);
 
-	uint idle = info->ms_since_user_input;
-	free (info);
+    uint idle = info->ms_since_user_input;
+    free (info);
 
-	return idle/1000;
+    return idle/1000;
 }
 
 } // namespace Psi
