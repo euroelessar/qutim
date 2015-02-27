d8a2ae3c3e17d019f3c8ef308fc45c2dfde58749
Ruslan Nigmatullin <euroelessar@yandex.ru>
2015-02-26 18:57:38 -0800

    [Multimedia] Move to QSoundEffect

--- plugins/mobility/src/multimediabackend/multimediasoundbackend.h
+++ plugins/mobility/src/multimediabackend/multimediasoundbackend.h
@@ -27,8 +27,8 @@
 #define MULTIMEDIASOUNDBACKEND_H
 
 #include <qutim/sound.h>
-#include <QAudioOutput>
-#include <QThread>
+#include <QCache>
+#include <QSoundEffect>
 
 using namespace qutim_sdk_0_3;
 
@@ -41,18 +41,10 @@ public:
     MultimediaSoundBackend();
     virtual void playSound(const QString &filename);
     virtual QStringList supportedFormats();
-};
 
-class MultimediaSound : public QObject
-{
-    Q_OBJECT
-public:
-    MultimediaSound(const QString &filename, QObject *parent = 0);
-public slots:
-    void play();
-    void finishedPlaying(QAudio::State state);
 private:
-    QString m_filename;
+    QCache<QString, QSoundEffect> m_cache;
+    QStringList m_formats;
 };
 }
 
