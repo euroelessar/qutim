6eb94d856e7d4b09a2609ba061a0814f3055966d
Ruslan Nigmatullin <euroelessar@yandex.ru>
2015-03-02 23:40:40 -0800

    [QuickChat] Added styling support for highlighter

--- plugins/quickchat/src/anchorshighlighter.cpp
+++ plugins/quickchat/src/anchorshighlighter.cpp
@@ -2,12 +2,19 @@
 #include <QAbstractTextDocumentLayout>
 #include <QMetaMethod>
 #include <QTimer>
+#include <QPalette>
+#include <QGuiApplication>
 
 namespace QuickChat {
 
 AnchorsHighlighter::AnchorsHighlighter() :
     QSyntaxHighlighter(static_cast<QTextDocument *>(nullptr))
 {
+    QPalette palette = QGuiApplication::palette();
+    m_activeColor = palette.link().color();
+    m_activeStyle = Qt::NoPen;
+    m_hoverColor = palette.link().color();
+    m_hoverStyle = Qt::SolidLine;
 }
 
 void AnchorsHighlighter::highlightBlock(const QString &text)
@@ -15,6 +22,14 @@ void AnchorsHighlighter::highlightBlock(const QString &text)
     Q_UNUSED(text);
     const QTextBlock block = currentBlock();
     const int position = m_position - block.position();
+
+    QTextCharFormat hoverFormat;
+    hoverFormat.setForeground(m_hoverColor);
+    hoverFormat.setUnderlineStyle(static_cast<QTextCharFormat::UnderlineStyle>(m_hoverStyle));
+
+    QTextCharFormat activeFormat;
+    activeFormat.setForeground(m_activeColor);
+    activeFormat.setUnderlineStyle(static_cast<QTextCharFormat::UnderlineStyle>(m_activeStyle));
 
     foreach (const QTextLayout::FormatRange &range, block.textFormats()) {
         const QString anchorHref = range.format.anchorHref();
@@ -27,15 +42,8 @@ void AnchorsHighlighter::highlightBlock(const QString &text)
             continue;
         }
 
-        QTextCharFormat format;
-        if (position >= range.start && position < range.start + range.length) {
-            format.setForeground(QColor(Qt::red));
-            format.setUnderlineStyle(QTextCharFormat::SingleUnderline);
-        } else {
-            format.setForeground(QColor(Qt::blue));
-            format.setUnderlineStyle(QTextCharFormat::NoUnderline);
-        }
-        setFormat(range.start, range.length, format);
+        const bool hover = position >= range.start && position < range.start + range.length;
+        setFormat(range.start, range.length, hover ? hoverFormat : activeFormat);
     }
 }
 
@@ -78,6 +86,70 @@ void AnchorsHighlighter::setPosition(int position)
 
     if (block.isValid() && oldBlock != block)
         rehighlightBlock(block);
+}
+
+QColor AnchorsHighlighter::activeColor() const
+{
+    return m_activeColor;
+}
+
+void AnchorsHighlighter::setActiveColor(QColor activeColor)
+{
+    if (m_activeColor == activeColor)
+        return;
+
+    m_activeColor = activeColor;
+    emit activeColorChanged(activeColor);
+
+    rehighlight();
+}
+
+Qt::PenStyle AnchorsHighlighter::activeStyle() const
+{
+    return m_activeStyle;
+}
+
+void AnchorsHighlighter::setActiveStyle(Qt::PenStyle activeStyle)
+{
+    if (m_activeStyle == activeStyle)
+        return;
+
+    m_activeStyle = activeStyle;
+    emit activeStyleChanged(activeStyle);
+
+    rehighlight();
+}
+
+QColor AnchorsHighlighter::hoverColor() const
+{
+    return m_hoverColor;
+}
+
+void AnchorsHighlighter::setHoverColor(QColor hoverColor)
+{
+    if (m_hoverColor == hoverColor)
+        return;
+
+    m_hoverColor = hoverColor;
+    emit hoverColorChanged(hoverColor);
+
+    rehighlight();
+}
+
+Qt::PenStyle AnchorsHighlighter::hoverStyle() const
+{
+    return m_hoverStyle;
+}
+
+void AnchorsHighlighter::setHoverStyle(Qt::PenStyle hoverStyle)
+{
+    if (m_hoverStyle == hoverStyle)
+        return;
+
+    m_hoverStyle = hoverStyle;
+    emit hoverStyleChanged(hoverStyle);
+
+    rehighlight();
 }
 
 QTextBlock AnchorsHighlighter::blockAt(int position)
--- plugins/quickchat/src/anchorshighlighter.h
+++ plugins/quickchat/src/anchorshighlighter.h
@@ -12,6 +12,10 @@ class AnchorsHighlighter : public QSyntaxHighlighter
     Q_OBJECT
     Q_PROPERTY(QQuickTextDocument* textDocument READ textDocument WRITE setTextDocument NOTIFY textDocumentChanged)
     Q_PROPERTY(int position READ potision WRITE setPosition NOTIFY positionChanged)
+    Q_PROPERTY(QColor activeColor READ activeColor WRITE setActiveColor NOTIFY activeColorChanged)
+    Q_PROPERTY(Qt::PenStyle activeStyle READ activeStyle WRITE setActiveStyle NOTIFY activeStyleChanged)
+    Q_PROPERTY(QColor hoverColor READ hoverColor WRITE setHoverColor NOTIFY hoverColorChanged)
+    Q_PROPERTY(Qt::PenStyle hoverStyle READ hoverStyle WRITE setHoverStyle NOTIFY hoverStyleChanged)
 public:
     explicit AnchorsHighlighter();
 
@@ -23,15 +27,35 @@ public:
     int potision() const;
     void setPosition(int position);
 
+    QColor activeColor() const;
+    void setActiveColor(QColor activeColor);
+
+    Qt::PenStyle activeStyle() const;
+    void setActiveStyle(Qt::PenStyle activeStyle);
+
+    QColor hoverColor() const;
+    void setHoverColor(QColor hoverColor);
+
+    Qt::PenStyle hoverStyle() const;
+    void setHoverStyle(Qt::PenStyle hoverStyle);
+
 signals:
     void textDocumentChanged(QQuickTextDocument *textDocument);
     void positionChanged(int position);
+    void activeColorChanged(QColor activeColor);
+    void activeStyleChanged(Qt::PenStyle activeStyle);
+    void hoverColorChanged(QColor hoverColor);
+    void hoverStyleChanged(Qt::PenStyle hoverStyle);
 
 private:
     QTextBlock blockAt(int position);
 
     QQuickTextDocument *m_textDocument = nullptr;
     int m_position = -1;
+    QColor m_activeColor;
+    Qt::PenStyle m_activeStyle;
+    QColor m_hoverColor;
+    Qt::PenStyle m_hoverStyle;
 };
 
 } // namespace QuickChat
